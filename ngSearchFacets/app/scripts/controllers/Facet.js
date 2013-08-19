5d88def53f9619bc86552599bcff5ee031f93428
Jeff Rodriguez <jeff@jeffrodriguez.com>
2013-08-18 21:02:33 -0700

    Working on faceted search

---
+++ ngSearchFacets/app/scripts/controllers/Facet.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ngSearchFacetsApp')
+  .controller('FacetCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
