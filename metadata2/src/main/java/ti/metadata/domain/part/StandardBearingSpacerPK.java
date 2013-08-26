a4a7424eb58667681f8dc351374306adb61c0577
Jeff Rodriguez <jeff@jeffrodriguez.com>
2013-08-26 02:30:13 -0700

    Removed metadata2

--- metadata2/src/main/java/ti/metadata/domain/part/StandardBearingSpacerPK.java
+++
@@ -1,6 +0,0 @@
-package ti.metadata.domain.part;
-import org.springframework.roo.addon.jpa.identifier.RooIdentifier;
-
-@RooIdentifier(dbManaged = true)
-public final class StandardBearingSpacerPK {
-}
