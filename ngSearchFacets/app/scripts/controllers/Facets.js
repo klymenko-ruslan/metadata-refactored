feed0bcf5a2d8705b24b77c2fdbbe276325d9b4c
Jeff Rodriguez <jeff@jeffrodriguez.com>
2013-08-26 04:54:11 -0700

    Added ngTable to bower and move search more into the search service.

--- ngSearchFacets/app/scripts/controllers/Facets.js
+++ ngSearchFacets/app/scripts/controllers/Facets.js
@@ -53,76 +53,7 @@ angular.module('ngSearchFacetsApp')
       }
 
       $scope.search = function() {
-          var queryString = null;
-          var searchRequest = {
-              from: 0,
-              size: 50,
-              facets: {
-                  "part_type": {
-                      terms: {
-                          field: "part_type",
-                          order: "term"
-                      }
-                  },
-                  "manufacturer_name.text": {
-                      terms: {
-                          field: "manufacturer_name.text",
-                          order: "term"
-                      }
-                  },
-                  "kit_type_name.text": {
-                      terms: {
-                          field: "kit_type_name.text",
-                          order: "term"
-                      }
-                  },
-                  "gasket_type_name.text": {
-                      terms: {
-                          field: "gasket_type_name.text",
-                          order: "term"
-                      }
-                  }
-              }
-          };
-
-          // Build the query
-          searchRequest.query = {
-              "bool": {
-                  "must":[]
-              }
-          };
-
-          // Facet Terms
-          if (angular.isObject($scope.facetFilters) && !jQuery.isEmptyObject($scope.facetFilters)) {
-              angular.forEach($scope.facetFilters, function(facetValue, facetName) {
-                  var term = {};
-                  term[facetName] = facetValue;
-
-                  searchRequest.query.bool.must.push({"term":term});
-              });
-          }
-
-          // Query string
-          if (angular.isString(queryString) && queryString.length > 0) {
-              searchRequest.query.bool.must.push({
-                  query: queryString,
-                  fields: [
-                      "manufacturer_part_number.autocomplete",
-                      "manufacturer_part_number.text",
-                      "manufacturer_name.autocomplete",
-                      "manufacturer_name.text",
-                      "part_type"
-                  ]
-              });
-          }
-
-          // Default query
-          if (searchRequest.query.bool.must.length == 0) {
-              searchRequest.query = {match_all: {}};
-          }
-
-          searchService(searchRequest).then(function(searchResults) {
-              $scope.searchRequest = searchRequest;
+          searchService(null, $scope.facetFilters).then(function(searchResults) {
               $scope.ngModel = searchResults.data;
           });
       };
