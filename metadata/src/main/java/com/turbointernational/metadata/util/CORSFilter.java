91fc94ad05f6ed7a0f6ca14d4998443e33131e66
Jeff Rodriguez <jeff@jeffrodriguez.com>
2013-09-10 20:08:57 -0700

    Added CORS filter.

---
+++ metadata/src/main/java/com/turbointernational/metadata/util/CORSFilter.java
@@ -0,0 +1,37 @@
+package com.turbointernational.metadata.util;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author jrodriguez
+ */
+public class CORSFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        
+        httpResponse.addHeader("Access-Control-Allow-Origin", "*");
+        httpResponse.addHeader("Access-Control-Allow-Headers", "Accept, Origin, X-Requested-With");
+            
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+    
+}
