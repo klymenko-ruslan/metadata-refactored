baf54af348fc3bbc482c0b3297782c957081c9bf
Jeff Rodriguez <jeff@jeffrodriguez.com>
2013-09-30 22:32:57 -0700

    ngRoute needs to be imported in this version of yeoman. It's also pickier.

--- metadata/yo/app/scripts/app.js
+++ metadata/yo/app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('ngMetaCrudApp', ['ui.bootstrap','restangular', 'ngTable', 'Mac', 'ui.select2'])
+angular.module('ngMetaCrudApp', ['ngRoute', 'ngTable', 'ui.bootstrap','restangular', 'Mac', 'ui.select2'])
     .config(function(RestangularProvider) {
         RestangularProvider.setBaseUrl('http://localhost:8080/');
         RestangularProvider.setDefaultHttpFields({accept:"application/json"})
@@ -10,32 +10,33 @@ angular.module('ngMetaCrudApp', ['ui.bootstrap','restangular', 'ngTable', 'Mac',
         })
     })
     .config(function ($routeProvider) {
-        $routeProvider
-            .when(['/part/:type'], {
-                templateUrl: 'views/part/PartList.html',
-                controller: 'PartListCtrl'
-            })
-            .when('/part/:type/form', {
-                templateUrl: 'views/part/PartForm.html',
-                controller: 'PartFormCtrl'
-            })
-            .when('/part/:type/:id/form', {
-                templateUrl: 'views/part/PartForm.html',
-                controller: 'PartFormCtrl'
-            })
-            .when('/part/:type/:id', {
-                templateUrl: 'views/part/PartDetail.html',
-                controller: 'PartDetailCtrl'
-            })
-            .when('/part/:type/:id/interchange/search', {
-                templateUrl: 'views/part/interchange/PartInterchangeSearch.html',
-                controller: 'PartInterchangeSearchCtrl'
-            })
-            .when('/part/:type/:id/bom/search', {
-                templateUrl: 'views/part/bom/PartBomSearch.html',
-                controller: 'PartBomSearchCtrl'
-            })
-            .otherwise({
-                redirectTo: '/part/'
-            });
+
+        $routeProvider.when('/part/:type', {
+            templateUrl: 'views/part/PartList.html',
+            controller: 'PartListCtrl'
+        });
+
+        $routeProvider.when('/part/:type/form', {
+            templateUrl: 'views/part/PartForm.html',
+            controller: 'PartFormCtrl'
+        });
+        $routeProvider.when('/part/:type/:id/form', {
+            templateUrl: 'views/part/PartForm.html',
+            controller: 'PartFormCtrl'
+        });
+        $routeProvider.when('/part/:type/:id', {
+            templateUrl: 'views/part/PartDetail.html',
+            controller: 'PartDetailCtrl'
+        });
+        $routeProvider.when('/part/:type/:id/interchange/search', {
+            templateUrl: 'views/part/interchange/PartInterchangeSearch.html',
+            controller: 'PartInterchangeSearchCtrl'
+        });
+        $routeProvider.when('/part/:type/:id/bom/search', {
+            templateUrl: 'views/part/bom/PartBomSearch.html',
+            controller: 'PartBomSearchCtrl'
+        });
+        $routeProvider.otherwise({
+            redirectTo: '/part/'
+        });
     });
