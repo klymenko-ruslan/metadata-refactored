01030a61be5852e85f1c55cd91cb8d8c02bda6aa
Jeff Rodriguez <jeff@jeffrodriguez.com>
2013-10-13 21:37:41 -0700

    Magento synchronization looking better.

--- metadata/src/main/java/com/turbointernational/metadata/domain/other/Manufacturer.java
+++ metadata/src/main/java/com/turbointernational/metadata/domain/other/Manufacturer.java
@@ -1,35 +1,41 @@
 package com.turbointernational.metadata.domain.other;
 import com.turbointernational.metadata.domain.type.ManufacturerType;
+import flexjson.JSONDeserializer;
+import flexjson.JSONSerializer;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import javax.persistence.Cacheable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityManager;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
+import javax.persistence.PersistenceContext;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
+import javax.persistence.Version;
 import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
-import org.springframework.roo.addon.json.RooJson;
+import org.springframework.transaction.annotation.Transactional;
 
 @Cacheable
 @Configurable
 @Entity
-@RooJpaActiveRecord
-@RooJson
 @Table(name="MANFR", uniqueConstraints=@UniqueConstraint(columnNames={"name"}))
 public class Manufacturer {
-
+    
+    //<editor-fold defaultstate="collapsed" desc="Properties">
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    
     @Column(nullable=false)
     private String name;
-
+    
     @OneToOne
     @JoinColumn(name="manfr_type_id", nullable=false)
     private ManufacturerType type;
@@ -37,36 +43,148 @@ public class Manufacturer {
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="parent_manfr_id")
     private Manufacturer parent;
-
+    
+    @Version
+    @Column(name = "version")
+    private Integer version;
+    
     public Long getId() {
         return id;
     }
-
+    
     public void setId(Long id) {
         this.id = id;
     }
-
+    
     public String getName() {
         return name;
     }
-
+    
     public void setName(String name) {
         this.name = name;
     }
-
+    
     public ManufacturerType getType() {
         return type;
     }
-
+    
     public void setType(ManufacturerType type) {
         this.type = type;
     }
-
+    
     public Manufacturer getParent() {
         return parent;
     }
-
+    
     public void setParent(Manufacturer parent) {
         this.parent = parent;
     }
+    
+    public Integer getVersion() {
+        return this.version;
+    }
+    
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="ActiveRecord">
+    @PersistenceContext
+    transient EntityManager entityManager;
+    
+    public static final EntityManager entityManager() {
+        EntityManager em = new Manufacturer().entityManager;
+        if (em == null) throw new IllegalStateException("Entity manager has not been injected (is the Spring Aspects JAR configured as an AJC/AJDT aspects library?)");
+        return em;
+    }
+    
+    public static long countManufacturers() {
+        return entityManager().createQuery("SELECT COUNT(o) FROM Manufacturer o", Long.class).getSingleResult();
+    }
+    
+    public static List<Manufacturer> findAllManufacturers() {
+        return entityManager().createQuery("SELECT o FROM Manufacturer o", Manufacturer.class).getResultList();
+    }
+    
+    public static Manufacturer findManufacturer(Long id) {
+        if (id == null) return null;
+        return entityManager().find(Manufacturer.class, id);
+    }
+    
+    public static List<Manufacturer> findManufacturerEntries(int firstResult, int maxResults) {
+        return entityManager().createQuery("SELECT o FROM Manufacturer o", Manufacturer.class).setFirstResult(firstResult).setMaxResults(maxResults).getResultList();
+    }
+
+    public static Manufacturer findByName(String name) {
+        return entityManager()
+            .createQuery("SELECT m FROM Manufacturer m WHERE m.name = :name", Manufacturer.class)
+            .setParameter("name", name)
+            .getSingleResult();
+    }
+    
+    @Transactional
+    public void persist() {
+        if (this.entityManager == null) this.entityManager = entityManager();
+        this.entityManager.persist(this);
+    }
+    
+    @Transactional
+    public void remove() {
+        if (this.entityManager == null) this.entityManager = entityManager();
+        if (this.entityManager.contains(this)) {
+            this.entityManager.remove(this);
+        } else {
+            Manufacturer attached = Manufacturer.findManufacturer(this.id);
+            this.entityManager.remove(attached);
+        }
+    }
+    
+    @Transactional
+    public void flush() {
+        if (this.entityManager == null) this.entityManager = entityManager();
+        this.entityManager.flush();
+    }
+    
+    @Transactional
+    public void clear() {
+        if (this.entityManager == null) this.entityManager = entityManager();
+        this.entityManager.clear();
+    }
+    
+    @Transactional
+    public Manufacturer merge() {
+        if (this.entityManager == null) this.entityManager = entityManager();
+        Manufacturer merged = this.entityManager.merge(this);
+        this.entityManager.flush();
+        return merged;
+    }
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Serialization">
+    public String toJson() {
+        return new JSONSerializer().exclude("*.class").serialize(this);
+    }
+    
+    public String toJson(String[] fields) {
+        return new JSONSerializer().include(fields).exclude("*.class").serialize(this);
+    }
+    
+    public static Manufacturer fromJsonToManufacturer(String json) {
+        return new JSONDeserializer<Manufacturer>().use(null, Manufacturer.class).deserialize(json);
+    }
+    
+    public static String toJsonArray(Collection<Manufacturer> collection) {
+        return new JSONSerializer().exclude("*.class").serialize(collection);
+    }
+    
+    public static String toJsonArray(Collection<Manufacturer> collection, String[] fields) {
+        return new JSONSerializer().include(fields).exclude("*.class").serialize(collection);
+    }
+    
+    public static Collection<Manufacturer> fromJsonArrayToManufacturers(String json) {
+        return new JSONDeserializer<List<Manufacturer>>().use(null, ArrayList.class).use("values", Manufacturer.class).deserialize(json);
+    }
+    //</editor-fold>
+    
 }
--- metadata/src/main/java/com/turbointernational/metadata/sync/MagentoSync.java
+++ metadata/src/main/java/com/turbointernational/metadata/sync/MagentoSync.java
@@ -1,22 +1,58 @@
 package com.turbointernational.metadata.sync;
 
+import com.google.code.magja.model.product.ProductLink;
+import com.turbointernational.metadata.domain.other.Manufacturer;
 import com.turbointernational.metadata.domain.part.Part;
 import com.turbointernational.metadata.magento.rest.MagentoRest;
 import com.turbointernational.metadata.magento.soap.MagentoSoap;
-import java.util.Date;
-import java.util.List;
+import java.util.Iterator;
+import java.util.Set;
 import net.sf.jsog.JSOG;
 import net.sf.jsog.client.UrlBuilder;
+import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.scribe.model.Token;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import static com.google.code.magja.model.product.ProductLink.LinkType.*;
 @Controller
 @RequestMapping("/other/sync")
 public class MagentoSync {
+    
+    private static JSOG partToProduct(Part part) {
+        
+        // Serialize to JSOG
+        JSOG partJsog = part.toJsog()
+        .put("sku", part.getId())
+        .put("attribute_set_id", part.getPartType().getMagentoAttributeSetId())
+        .put("short_description", part.getDescription()) // Required
+        .put("type_id","simple") // Required
+        .put("status", "1") // Required
+        .put("visibility","4") // Required
+        ;
+        
+        if (StringUtils.isBlank(part.getName())) {
+            partJsog.put("name", part.getManufacturer().getName() + " " + part.getManufacturerPartNumber());
+        }
+        
+        if (StringUtils.isBlank(part.getDescription())) {
+            partJsog.put("description", "no description given");
+        }
+                
+//        .put("name", "my name") // Required
+//        .put("description","my desc") // Required
+                                
+                                
+//        .put("tax_class_id","0") // Required
+//        .put("weight", "0.0") // Required
+//        .put("price", "42.00") // Required
+        
+        return partJsog;
+    }
     
     //
     // REST
@@ -53,106 +89,157 @@ public class MagentoSync {
     protected MagentoSoap soap() {
         return new MagentoSoap(soapApiUsername, soapApiPassword, soapApiUrl);
     }
-
-    public void synchronize(Date lastUpdated) {
-        int pageSize = 1000;
-        int page = 0;
-        List<Part> parts;
-        do {
-            parts = Part.getPartsUpdatedAfter(lastUpdated, page * pageSize, pageSize);
-
-            for (Part part : parts) {
-                
-                // Remove part if non-existent or inactive
-                if (part.getInactive() || part == null) {
-                    deleteProduct(part);
-                
-                    // Otherwise update part
-                } else {
-                    updateProduct(part);
-                }
+    
+    @Transactional
+    @RequestMapping(value="/part")
+    @ResponseBody
+    public String syncPart(@RequestParam long id) {
+        Part part = Part.findPart(id);
+        
+        // Get the existing product if it exists
+        JSOG productJsog = rest().getProductBySku(part.getId().toString());
+        
+        // Create or update the product
+        if (productJsog == null) {
+            
+            // Create the product
+            productJsog = partToProduct(part);
+            int productId = rest().createProduct(productJsog);
+            part.setMagentoProductId(productId);
+            part.merge();
+            part.flush();
+            
+        } else {
+        
+            // Set the magento product ID it hasn't been
+            if (part.getMagentoProductId() == null) {
+                int productId = productJsog.get("entity_id").getIntegerValue();
+                part.setMagentoProductId(productId);
+                part.merge();
+                part.flush();
+            }
+            
+            // Get the current product version
+            Integer productVersion = null;
+            if (productJsog.hasKey("version")) {
+                productJsog.get("version").getIntegerValue();
             }
 
-            page++;
-
-        } while (parts.size() >= pageSize);
-    }
-    
-    @RequestMapping(value="/part", headers = "Accept=application/json")
-    @ResponseBody
-    public String addPart(@RequestParam long id) {
-        return addProduct(Part.findPart(id));
-    }
-    
-    @RequestMapping(value="/parts", headers = "Accept=application/json")
-    @ResponseBody
-    public void addAllParts() {
-        for (Part part : Part.findAll()) {
-            addProduct(part);
-        }
-    }
-
-    
-    private void updateProduct(Part part) {
-
-        // Get the part as JSOG, delete/transform the values for Magento
-        JSOG partJsog = part.toJsog();
-        partJsog.remove("id");
-
-        // Get the current product
-        JSOG originalProduct = getProduct(part);
-
-        // Merge in the current part's properties
-        JSOG updatedProduct = originalProduct.clone().merge(partJsog);
-
-        // If they're different, update the part
-        if (!originalProduct.equals(updatedProduct)) {
-            rest().updateProduct(originalProduct.get("id").getIntegerValue(), partJsog);
-        }
-
-    }
-
-    private String addProduct(Part part) {
-        
-        // Serialize to JSOG as in part update
-        JSOG partJsog = part.toJsog()
-        .put("sku", part.getId())
-        .put("attribute_set_id", part.getPartType().getMagentoAttributeSetId())
-        .put("short_description", part.getDescription()) // Required
-        .put("type_id","simple") // Required
-        .put("status", "1") // Required
-        .put("visibility","4") // Required
-        ;
-        
-        if (StringUtils.isBlank(part.getName())) {
-            partJsog.put("name", part.getManufacturer().getName() + " " + part.getManufacturerPartNumber());
+            // Update if it's different from the current part version
+            if (!ObjectUtils.equals(part.getVersion(), productVersion)) {
+                productJsog.merge(partToProduct(part));
+                rest().updateProduct(part.getMagentoProductId(), productJsog);
+            }
         }
         
-        if (StringUtils.isBlank(part.getDescription())) {
-            partJsog.put("description", "no description given");
-        }
-                
-//        .put("name", "my name") // Required
-//        .put("description","my desc") // Required
-                                
-                                
-//        .put("tax_class_id","0") // Required
-//        .put("weight", "0.0") // Required
-//        .put("price", "42.00") // Required
-
+        // Update the product category
+        setProductCategory(part);
         
-        // POST request to create product
-        return rest().createProduct(partJsog).toString();
-    }
-
-    private void deleteProduct(Part part) {
-        // Get the part ID
-        int partId = part.getMagentoProductId();
-        // Send DELETE request for the part
-        rest().deleteProduct(partId);
+        // Update interchange parts
+        updateProductLinks(part);
+        
+        return part.getMagentoProductId().toString();
     }
 
     public JSOG getProduct(Part part) {
         return rest().getProductById(part.getMagentoProductId());
     }
+    
+    private void setProductCategory(Part part) {
+        Integer partMagentoCategoryId = part.getPartType().getMagentoCategoryId();
+        
+        // Handle null category ID
+        if (partMagentoCategoryId == null) {
+            throw new IllegalStateException("No magento category id assigned for part #" + part.getId());
+        }
+        
+        // Get the categories currently on the product
+        Set<Integer> currentCategories = rest().getCategories(part.getMagentoProductId());
+        
+        // Delete any wrong ones
+        Iterator<Integer> categoryIt = currentCategories.iterator();
+        while (categoryIt.hasNext()) {
+            Integer categoryId = categoryIt.next();
+            
+            if (ObjectUtils.notEqual(partMagentoCategoryId, categoryId)) {
+                rest().deleteCategory(part.getMagentoProductId(), categoryId);
+                categoryIt.remove();
+            }
+        }
+        
+        // Add the category if we need to
+        if (currentCategories.isEmpty()) {
+            rest().addCategory(part.getMagentoProductId(), partMagentoCategoryId);
+        }
+    }
+    
+    private void updateProductLinks(Part part) {
+        Manufacturer mfrTi = Manufacturer.findByName("Turbo International");
+        
+        Set<Part> interchangeParts = part.getInterchangePartsForManufacturer(mfrTi);
+        
+        // Get the existing product links
+        linkLoop: for (ProductLink link : soap().getProductLinks(part.getMagentoProductId())) {
+            System.out.println(ToStringBuilder.reflectionToString(link));
+            switch (link.getLinkType()) {
+                
+                // Cross-sells are used for interchangeable parts
+                case CROSS_SELL: {
+                    
+                    // Remove parts from interchangeParts if a link already exists
+                    Iterator<Part> interchangeIt = interchangeParts.iterator();
+                    while (interchangeIt.hasNext()) {
+                        Part interchangePart = interchangeIt.next();
+                        
+                        if (ObjectUtils.equals(interchangePart.getId().toString(), link.getSku())) {
+                            interchangeIt.remove();
+                            break linkLoop;
+                        }
+                    }
+                    
+                    // The link is no longer valid
+                    soap().deleteProductLink(part.getMagentoProductId(), link.getId(), ProductLink.LinkType.CROSS_SELL);
+                }
+            }
+        }
+        
+        // Create any missing interchange links
+        for (Part interchangePart : interchangeParts) {
+            soap().addProductLink(part.getMagentoProductId(), interchangePart.getMagentoProductId(), CROSS_SELL);
+        }
+    }
+    
+//    @RequestMapping(value="/parts", headers = "Accept=application/json")
+//    @ResponseBody
+//    public void addAllParts() {
+//        for (Part part : Part.findAll()) {
+//            addProduct(part);
+//        }
+//    }
+    
+    
+
+//    public void synchronize(Date lastUpdated) {
+//        int pageSize = 1000;
+//        int page = 0;
+//        List<Part> parts;
+//        do {
+//            parts = Part.getPartsUpdatedAfter(lastUpdated, page * pageSize, pageSize);
+//
+//            for (Part part : parts) {
+//                
+//                // Remove part if non-existent or inactive
+//                if (part.getInactive() || part == null) {
+//                    deleteProduct(part);
+//                
+//                    // Otherwise update part
+//                } else {
+//                    updateProduct(part);
+//                }
+//            }
+//
+//            page++;
+//
+//        } while (parts.size() >= pageSize);
+//    }
 }
