31bfd3380d4618e38d7334c9f1326e3c95cb4e68
Jeff Rodriguez <jeff@jeffrodriguez.com>
2013-11-17 20:38:24 -0800

    Cleaned up PartDetailCtrl

--- metadata/yo/app/scripts/controllers/part/PartDetail.js
+++ metadata/yo/app/scripts/controllers/part/PartDetail.js
@@ -2,10 +2,10 @@
 
 angular.module('ngMetaCrudApp')
     .controller('PartDetailCtrl', function ($scope, $log, $location, $routeParams, ngTableParams, restService) {
-        $scope.partId = $routeParams.id;
-        $scope.partType = $routeParams.type;
+      $scope.partId = $routeParams.id;
+      $scope.partType = $routeParams.type;
 
-        $scope.part = restService.findPart($scope.partId, {fields: 'bom,interchange.parts'});
+      $scope.part = restService.findPart($scope.partId, {fields: 'bom,interchange.parts'});
 
 
       $scope.interchangeTableParams = new ngTableParams({
@@ -14,26 +14,20 @@ angular.module('ngMetaCrudApp')
         counts: [10, 25, 50, 100]
       });
 
-      $log.log($scope.interchangeTableParams);
-
-
       $scope.bomTableParams = new ngTableParams({
         page: 1,
         count: 10,
         counts: [10, 25, 50, 100]
       });
 
-        $scope.part.then(function(part) {
-          $scope.interchangeTableParams.total(part.interchange.parts.length);
-          $scope.bomTableParams.total(part.bom.length);
+      $scope.part.then(function(part) {
+        console.log("Loaded part: " + part.id);
 
-                console.log("Loaded part: " + part.id);
-
-            // Make sure we're using the correct part type
-            $scope.partType = part.partType.typeName;
-        }, function(response) {
-            console.error("Could not get part data from server.");
-        });
+        // Make sure we're using the correct part type
+        $scope.partType = part.partType.typeName;
+      }, function(response) {
+        console.error("Could not get part data from server.");
+      });
 
       $scope.rowClick = function(partType, partId) {
         $location.path("/part/" + partType + "/" + partId);
