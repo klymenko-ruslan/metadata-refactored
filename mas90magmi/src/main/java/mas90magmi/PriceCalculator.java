19506a3af2a94e62b9d4d1146033c159909bfd86
Jeff Rodriguez <jeff@jeffrodriguez.com>
2013-11-30 12:32:44 -0800

    Latest pass on price calculator.

--- mas90magmi/src/main/java/mas90magmi/PriceCalculator.java
+++ mas90magmi/src/main/java/mas90magmi/PriceCalculator.java
@@ -1,14 +1,59 @@
 package mas90magmi;
 
 import java.math.BigDecimal;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import mas90magmi.Pricing.PriceBreak;
 
 /**
  *
  * @author jrodriguez
  */
 public class PriceCalculator {
+    
+    public static class CalculatedPrice {
+        private String priceLevel;
+        private int breakLevel;
+        private int quantity;
+        private BigDecimal price;
+
+        public CalculatedPrice(String priceLevel, int breakLevel, int quantity, BigDecimal price) {
+            this.priceLevel = priceLevel;
+            this.breakLevel = breakLevel;
+            this.quantity = quantity;
+            this.price = price;
+        }
+
+        /**
+         * @return the priceLevel
+         */
+        public String getPriceLevel() {
+            return priceLevel;
+        }
+
+        /**
+         * @return the breakLevel
+         */
+        public int getBreakLevel() {
+            return breakLevel;
+        }
+
+        /**
+         * @return the quantity
+         */
+        public int getQuantity() {
+            return quantity;
+        }
+
+        /**
+         * @return the price
+         */
+        public BigDecimal getPrice() {
+            return price;
+        }
+    }
     
     private final Map<String, Pricing> priceLevelPricings;
 
@@ -37,5 +82,41 @@ public class PriceCalculator {
         }
         
         return prices;
+    }
+    
+    public Pricing getPriceLevelPricing(String priceLevel, ItemPricing item) {
+        
+        // Get the item-level pricing if there is one
+        Pricing itemPriceLevelPricing = item.getPriceLevelPricings().get(priceLevel);
+        if (itemPriceLevelPricing != null) {
+            return itemPriceLevelPricing;
+        }
+
+        // Fall back to price-level pricing
+        return priceLevelPricings.get(priceLevel);
+    }
+    
+    public Iterator<CalculatedPrice> calculatePriceBreaks(final String priceLevel, final ItemPricing item) {
+        final AtomicInteger nextBreakLevel = new AtomicInteger(0);
+        
+        final Pricing priceLevelPricing = getPriceLevelPricing(priceLevel, item);
+        
+        return new Iterator<CalculatedPrice>() {
+            public boolean hasNext() {
+                PriceBreak priceBreak = priceLevelPricing.getPriceBreak(nextBreakLevel.get());
+                return priceBreak.getQuantity() > 0;
+            }
+
+            public CalculatedPrice next() {
+                PriceBreak priceBreak = priceLevelPricing.getPriceBreak(nextBreakLevel.get());
+                BigDecimal price = priceLevelPricing.applyPriceBreak(nextBreakLevel.get(), item.getStandardPrice());
+                
+                return new CalculatedPrice(priceLevel, nextBreakLevel.getAndIncrement(), priceBreak.getQuantity(), price);
+            }
+
+            public void remove() {
+                throw new UnsupportedOperationException("Not supported.");
+            }
+        };
     }
 }
