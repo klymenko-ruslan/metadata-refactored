34b709a6c87ea38e21467f5cc716e584c35fe1ba
jeffwesson <jeff@jeffwesson.com>
2013-12-14 20:18:05 -0800

    Closes #134 and updates to bootstrap3, etc.

--- metadata/yo/app/scripts/controllers/part/PartList.js
+++ metadata/yo/app/scripts/controllers/part/PartList.js
@@ -1,22 +1,22 @@
 'use strict';
 
 angular.module('ngMetaCrudApp')
-  .controller('PartListCtrl', function ($scope, $location, $log, $rootScope, $modal) {
-        $scope.onAction = function(action, partId, partType) {
+    .controller('PartListCtrl', function ($scope, $location, $log, $rootScope, $modal) {
+        $scope.onAction = function (action, partId, partType) {
             switch (action) {
                 case "View":
-                    $location.path('/part/' + partType  + '/' +  partId);
+                    $location.path('/part/' + partType + '/' + partId);
                     return;
                 case "Edit":
-                    $location.path('/part/' + partType  + '/' +  partId + '/form');
+                    $location.path('/part/' + partType + '/' + partId + '/form');
                     return;
             }
         };
 
-      $scope.createPart = function() {
-        var modalInstance = $modal.open({
-          templateUrl: '/views/part/PartCreateModal.html',
-          controller: 'PartCreateModalCtrl'
-        });
-      };
-  });
+        $scope.createPart = function () {
+            var modalInstance = $modal.open({
+                templateUrl: '/views/part/PartCreateModal.html',
+                controller: 'PartCreateModalCtrl'
+            });
+        };
+    });
