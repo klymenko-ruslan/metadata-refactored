1defffac227e2f9eac87e5b246182bda22618ac4
Jeff Rodriguez <jeff@jeffrodriguez.com>
2014-03-03 08:09:19 -0800

    Moved admin link methods into metanav.

--- metadata/yo/app/scripts/directives/metanav.js
+++ metadata/yo/app/scripts/directives/metanav.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('ngMetaCrudApp')
-  .directive('metanav', function () {
+  .directive('metanav', function ($dialogs, $log, gToast, Restangular) {
     return {
       transclude: true,
       template:
@@ -86,6 +86,78 @@ angular.module('ngMetaCrudApp')
           '  </li>' +
           '</ul>' +
           '</div>',
-      restrict: 'E'
+      restrict: 'E',
+      controller: function($scope) {
+
+
+        $scope.reindexTurbos = function() {
+          $dialogs.confirm(
+                  "Reindex all part turbos?",
+                  "You need to run this if changes have been made directly to the database. Proceed?").result.then(
+              function() {
+                // Yes
+                Restangular.all('part/all/indexTurbos').get().then(
+                    function() {
+                      // Success
+                      gToast.open("Indexing started, check the server log for progress.");
+                    },
+                    function(response) {
+                      // Error
+                      $dialogs.error(
+                          "Could not index search engine data.",
+                          "Here's the error: <pre>" + response.status +"</pre>");
+                    });
+              },
+              function() {
+                // No
+              });
+        }
+
+        $scope.reindexSearch = function() {
+          $dialogs.confirm(
+                  "Reindex search engine data?",
+                  "You need to run this if changes have been made directly to the database. Proceed?").result.then(
+              function() {
+                // Yes
+                Restangular.all('part/all/indexSearch').post().then(
+                    function() {
+                      // Success
+                      gToast.open("Indexing started, check the server log for progress.");
+                    },
+                    function(response) {
+                      // Error
+                      $dialogs.error(
+                          "Could not index search engine data.",
+                          "Server said: <pre>" + JSON.stringify(response.data) + "</pre>");
+                    });
+              },
+              function() {
+                // No
+              });
+        }
+
+        $scope.clearHibernate = function() {
+          $dialogs.confirm(
+                  "Clear Hibernate cache?",
+                  "You need to run this if changes have been made directly to the database. Proceed?").result.then(
+              function() {
+                // Yes
+                Restangular.one('hibernate/clear').get().then(
+                    function() {
+                      // Success
+                      gToast.open("Hibernate cache cleared.");
+                    },
+                    function(response) {
+                      // Error
+                      $dialogs.error(
+                          "Could not index search engine data.",
+                          "Here's the error: <pre>" + response.status +"</pre>");
+                    });
+              },
+              function() {
+                // No
+              });
+        }
+      }
     };
   });
