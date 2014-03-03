30d77ac1b1fad9042251138a71facabe7e99967c
Jeff Rodriguez <jeff@jeffrodriguez.com>
2014-03-02 16:27:43 -0800

    Cleaning up directives.

--- metadata/yo/app/scripts/directives/component/picker.js
+++ metadata/yo/app/scripts/directives/component/picker.js
@@ -9,9 +9,94 @@ angular.module('ngMetaCrudApp')
                 "ngModel": '@',
                 "required": '@'
             },
-            templateUrl: '/views/component/Picker.html',
             restrict: 'E',
-            controller: 'PickerCtrl',
+            template:
+                '<div class="picker row">' +
+                '  <div class="col-xs-11">' +
+                '    <select ng-model="selection.id"' +
+                '    ng-options="item.id as item.name for item in items | orderBy: \'name\'"' +
+                '    ng-disabled="items == null"' +
+                '    placeholder="Choose..." class="form-control input-sm"></select>' +
+                '  </div>' +
+                '  <div class="col-xs-1">' +
+                '    <button ng-click="selection.id=\'0\'" ng-disabled="!selection.id"' +
+                '    class="btn btn-link btn-sm">' +
+                '      <i class="fa fa-times"></i>' +
+                '    </button>' +
+                '  </div>' +
+                '</div>',
+            controller: function ($log, $scope, Restangular, $parse) {
+
+              var getter = $parse($scope.ngModel);
+              var setter = getter.assign;
+
+              // Fetch the items if we got a path
+              if ($scope.path != null) {
+                Restangular.all($scope.path).getList().then(function (items) {
+                  $scope.items = items;
+                }, function (response) {
+                  console.error("Failed to load items from " + $scope.path);
+                });
+              }
+
+              /**
+               * Current selection
+               * @type {{}}
+               */
+              $scope.selection = {};
+
+              // Setup the initial selection
+              var initialValue = getter($scope.$parent);
+              if (angular.isObject(initialValue)) {
+//        $log.log($scope.ngModel, "Picker Initial Value", initialValue);
+                $scope.selection.id = initialValue.id;
+              }
+
+              /**********\
+               * Watches
+               **********/
+
+              /**
+               * Watches for external changes.
+               */
+              $scope.$watch(
+                  function () {
+                    return getter($scope.$parent);
+                  },
+                  function (newExternalValue, oldExternalValue) {
+                    if (newExternalValue === oldExternalValue) return;
+
+//            $log.log("Picker getter", $scope.ngModel, newExternalValue, oldExternalValue);
+
+                    if (angular.isObject(newExternalValue)) {
+                      $scope.selection.id = newExternalValue.id;
+                    } else {
+                      $scope.selection.id = null;
+                    }
+                  });
+
+              /**
+               * Watches for and propagates selection changes.
+               */
+              $scope.$watch('selection.id', function (newSelectedValue, oldSelectedValue) {
+                if (newSelectedValue === oldSelectedValue) return;
+
+                if ($scope.items == null) return;
+
+//        $log.log("Picker selection.id", $scope.ngModel,  newSelectedValue, oldSelectedValue);
+
+                if (newSelectedValue == null) {
+                  setter($scope.$parent, null);
+                } else {
+                  var selectedItem = _.find($scope.items, function (item) {
+                    return $scope.selection.id == item.id;
+                  });
+
+                  setter($scope.$parent, selectedItem);
+                }
+              });
+
+            },
             link: function (scope, element, attrs) {
 //              scope.ngModelAttr = attrs.ngModel;
             }
--- metadata/yo/app/scripts/directives/component/searchFacets.js
+++ metadata/yo/app/scripts/directives/component/searchFacets.js
@@ -4,6 +4,55 @@ angular.module('ngMetaCrudApp')
     .directive('searchFacets', function () {
         return {
             restrict: 'E',
+            controller:  function ($scope) {
+
+              // Filter down to facets with totals greater than zero
+              $scope.search.filterFacets = function (facets) {
+                var results = {};
+
+                angular.forEach(facets, function (facet, facetName) {
+                  if (facet.total > 0) {
+                    results[facetName] = facet;
+                  }
+                });
+
+                return results;
+              };
+
+              // Filter down to terms with counts greater than zero.
+              $scope.filterTerms = function (facetName, facet) {
+                var results = [];
+
+                angular.forEach(facet.terms, function (term) {
+                  if (term.count > 0) {
+                    term.facetName = facetName;
+                    results.push(term);
+                  }
+                });
+
+                return results;
+              };
+
+              $scope.isFacetSelected = function (facetName) {
+                return facetName in $scope.search.facetFilters
+                    && angular.isDefined($scope.search.facetFilters[facetName])
+                    && $scope.search.facetFilters[facetName] != null;
+              };
+
+              $scope.isTermSelected = function (facetName, term) {
+                return $scope.isFacetSelected(facetName)
+                    && $scope.search.facetFilters[facetName] == term.term;
+              };
+
+              $scope.setFacetFilter = function (facetName, term) {
+                console.log("Filtering on " + facetName + ": " + term);
+                $scope.search.facetFilters[facetName] = term;
+              };
+
+              $scope.removeFacetFilter = function (facetName) {
+                delete $scope.search.facetFilters[facetName];
+              }
+            },
             templateUrl: '/views/component/SearchFacets.html'
         };
     });
