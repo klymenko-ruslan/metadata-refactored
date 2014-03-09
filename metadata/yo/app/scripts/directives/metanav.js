7fc1d90be51df7704cbd010803967f4358c11fbf
jeffwesson <jeff@jeffwesson.com>
2014-03-08 17:25:01 -0800

    cleaning up metadata, WIP modal focus outlines

--- metadata/yo/app/scripts/directives/metanav.js
+++ metadata/yo/app/scripts/directives/metanav.js
@@ -1,91 +1,10 @@
 'use strict';
 
 angular.module('ngMetaCrudApp')
-  .directive('metanav', function () {
-    return {
-      transclude: true,
-      template:
-          '<div class="row">' +
-
-           // Transcluded
-          '<ul class="nav nav-pills" ng-transclude  style="display: inline-block;">' +
-          '</ul>' +
-
-          '<ul class="nav nav-pills" style="display: inline-block;">' +
-
-           // Parts
-          '  <li>' +
-          '    <a authorize="ROLE_READ"' +
-          '       ng-href="#/"' +
-          '       class="btn btn-default">' +
-          '      <i class="fa fa-bars"></i>' +
-          '    Part List' +
-          '    </a>' +
-          '  </li>' +
-
-           // Indexing and Cache
-          '  <li class="dropdown" authorize="ROLE_ADMIN">' +
-          '    <a class="btn btn-warning dropdown-toggle" data-toggle="dropdown" href="#">' +
-          '      Indexing and Cache <span class="caret"></span>' +
-          '    </a>' +
-          '    <ul class="dropdown-menu">' +
-          '      <li>' +
-          '        <a ng-click="reindexTurbos()"' +
-          '           class="btn">' +
-          '          <i class="fa fa-cogs"></i>' +
-          '          Reindex Turbos' +
-          '        </a>' +
-          '      </li>' +
-          '      <li>' +
-          '        <a ng-click="reindexSearch()"' +
-          '           class="btn">' +
-          '          <i class="fa fa-cogs"></i>' +
-          '          Reindex Search' +
-          '        </a>' +
-          '      </li>' +
-          '      <li>' +
-          '        <a ng-click="clearHibernate()"' +
-          '           class="btn">' +
-          '          <i class="fa fa-cogs"></i>' +
-          '          Clear Hibernate Cache' +
-          '        </a>' +
-          '      </li>' +
-          '    </ul>' +
-          '  </li>' +
-
-           // Security
-          '  <li class="dropdown" authorize="ROLE_ADMIN">' +
-          '    <a class="btn btn-default dropdown-toggle" data-toggle="dropdown">' +
-          '      Security <span class="caret"></span>' +
-          '    </a>' +
-          '    <ul class="dropdown-menu">' +
-          '     <li>' +
-          '       <a ng-href="#/security/groups/"' +
-          '          class="btn">' +
-          '         <i class="fa fa-group"></i>' +
-          '         Groups' +
-          '       </a>' +
-          '     </li>' +
-          '     <li>' +
-          '       <a ng-href="#/security/users/"' +
-          '          class="btn">' +
-          '         <i class="fa fa-user"></i>' +
-          '         Users' +
-          '       </a>' +
-          '     </li>' +
-          '    </ul>' +
-          '  </li>'+
-
-           // My Account
-          '  <li>' +
-          '    <a ng-href="#/security/me"' +
-          '       class="btn btn-primary">' +
-          '      <i class="fa fa-smile-o"></i>' +
-          '      My Account' +
-          '    </a>' +
-          '  </li>' +
-          '</ul>' +
-          '</div>',
-      restrict: 'E'
-    };
-  });
+    .directive('metanav', function () {
+        return {
+            transclude: true,
+            templateUrl: '/views/component/Metanav.html',
+            restrict: 'E'
+        };
+    });
