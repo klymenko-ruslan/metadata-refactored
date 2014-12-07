3151ac44c45b8d05722b124fa45023bc2ba68d54
Jeff Rodriguez <jeff@jeffrodriguez.com>
2014-12-06 18:05:05 -0800

    Fixed tests for metadata.

--- metadata/src/test/java/com/turbointernational/metadata/magmi/MagmiDataFinderTest.java
+++ metadata/src/test/java/com/turbointernational/metadata/magmi/MagmiDataFinderTest.java
@@ -1,24 +1,11 @@
 
 package com.turbointernational.metadata.magmi;
 
-import com.google.common.collect.Lists;
-import com.turbointernational.metadata.domain.other.Manufacturer;
-import com.turbointernational.metadata.domain.other.TurboModel;
-import com.turbointernational.metadata.domain.other.TurboType;
-import com.turbointernational.metadata.domain.part.bom.BOMItem;
-import com.turbointernational.metadata.domain.part.types.Cartridge;
-import com.turbointernational.metadata.domain.part.types.Turbo;
-import com.turbointernational.metadata.domain.type.CoolType;
-import com.turbointernational.metadata.domain.type.ManufacturerType;
-import com.turbointernational.metadata.domain.type.PartType;
-import com.turbointernational.metadata.magmi.dto.MagmiBomItem;
-import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.*;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 
@@ -27,7 +14,7 @@ import org.springframework.test.context.junit4.AbstractTransactionalJUnit4Spring
  * @author jrodriguez
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration
+@ContextConfiguration(locations = "classpath:WEB-INF/applicationContext.xml")
 public class MagmiDataFinderTest extends AbstractTransactionalJUnit4SpringContextTests {
     
     MagmiDataFinder instance;
