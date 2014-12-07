cf2767f09badddacb242d84c55a7e8fd2b00aed9
Jeff Rodriguez <jeff@jeffrodriguez.com>
2014-12-06 19:29:21 -0800

    A little more help for CORS.

--- metadata/src/main/java/com/turbointernational/metadata/util/CORSFilter.java
+++ metadata/src/main/java/com/turbointernational/metadata/util/CORSFilter.java
@@ -9,6 +9,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  *
@@ -23,7 +24,7 @@ public class CORSFilter implements Filter {
         
         httpResponse.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
         httpResponse.addHeader("Access-Control-Allow-Headers", httpRequest.getHeader("Access-Control-Request-Headers"));
-        httpResponse.addHeader("Access-Control-Allow-Origin", httpRequest.getHeader("Origin"));
+        httpResponse.addHeader("Access-Control-Allow-Origin", StringUtils.defaultIfBlank(httpRequest.getHeader("Origin"), "*"));
         httpResponse.addHeader("Access-Control-Allow-Credentials", "true");
             
         chain.doFilter(request, response);
