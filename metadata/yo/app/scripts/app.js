3921dafe539ae2302d148aad4e3ecbff7d15eaf5
salmanmalik90 <salman-malik@windowslive.com>
2015-05-09 04:58:28 -0700

    New HTML Page for viewing a SalesNote

--- metadata/yo/app/scripts/app.js
+++ metadata/yo/app/scripts/app.js
@@ -1,110 +1,130 @@
 'use strict';
 
-angular.module('ngMetaCrudApp', ['ngRoute', 'ngTable', 'ui.bootstrap', 'restangular', 'dialogs', 'gToast'])
+angular.module('ngMetaCrudApp', ['ngRoute', 'ngTable', 'ui.bootstrap',
+    'restangular', 'dialogs', 'gToast'
+  ])
   //.constant('METADATA_BASE', '/metadata/')
   .constant('METADATA_BASE', 'http://192.168.42.10:8080/metadata/')
-  .config(function ($locationProvider, $httpProvider, $routeProvider, RestangularProvider, METADATA_BASE) {
-        $httpProvider.interceptors.push('loginRequiredInterceptor');
-    
-        RestangularProvider.setBaseUrl(METADATA_BASE);
-        RestangularProvider.setParentless(true);
-        RestangularProvider.setDefaultHttpFields({withCredentials: true});
-        RestangularProvider.setDefaultHeaders({'Content-Type': 'text/plain'});
-        RestangularProvider.setResponseExtractor(function (response) {
-            return response;
-          });
+  .config(function($locationProvider, $httpProvider, $routeProvider,
+    RestangularProvider, METADATA_BASE) {
+    $httpProvider.interceptors.push('loginRequiredInterceptor');
 
-        $locationProvider.html5Mode(true);
+    RestangularProvider.setBaseUrl(METADATA_BASE);
+    RestangularProvider.setParentless(true);
+    RestangularProvider.setDefaultHttpFields({
+      withCredentials: true
+    });
+    RestangularProvider.setDefaultHeaders({
+      'Content-Type': 'text/plain'
+    });
+    RestangularProvider.setResponseExtractor(function(response) {
+      return response;
+    });
 
-        // Parts
-        $routeProvider.when('/part/list', {
-            templateUrl: 'views/part/PartList.html',
-            controller: 'PartListCtrl'
-          });
-        $routeProvider.when('/part/createByPartTypeId/:typeId', {
-            templateUrl: 'views/part/PartForm.html',
-            controller: 'PartFormCtrl',
-            resolve: {
-              partTypes: ['PartTypes', function(PartTypes) {
-                return PartTypes.getPromise();
-              }]
-            }
-          });
-        $routeProvider.when('/part/:id/form', {
-            templateUrl: 'views/part/PartForm.html',
-            controller: 'PartFormCtrl'
-          });
-        $routeProvider.when('/part/:id/interchange/search', {
-          templateUrl: 'views/part/interchange/PartInterchangeSearch.html',
-          controller: 'PartInterchangeSearchCtrl'
-        });
-        $routeProvider.when('/part/Kit/:id/component/search', {
-          templateUrl: '../views/part/KitComponentSearch.html',
-          controller: 'KitComponentSearchCtrl'
-        });
-        $routeProvider.when('/part/:id/bom/search', {
-            templateUrl: 'views/part/bom/PartBomSearch.html',
-            controller: 'PartBomSearchCtrl'
-        });
-        $routeProvider.when('/part/:id/bom/:bomId/search', {
-          templateUrl: 'views/part/bom/BomAlternateSearch.html',
-          controller: 'BomAlternateSearchCtrl'
-        });
-        $routeProvider.when('/part/:id/ancestors', {
-          templateUrl: 'views/part/PartAncestors.html',
-          controller: 'PartAncestorsCtrl'
-        });
-        $routeProvider.when('/part/:id', {
-          templateUrl: 'views/part/PartDetail.html',
-          controller: 'PartDetailCtrl'
-        });
+    $locationProvider.html5Mode(true);
 
-        // Turbo Models
-        $routeProvider.when('/other/turboModels', {
-          templateUrl: 'views/other/TurboModels.html',
-          controller: 'TurboModelsCtrl'
-        });
+    // Parts
+    $routeProvider.when('/part/list', {
+      templateUrl: 'views/part/PartList.html',
+      controller: 'PartListCtrl'
+    });
+    $routeProvider.when('/part/createByPartTypeId/:typeId', {
+      templateUrl: 'views/part/PartForm.html',
+      controller: 'PartFormCtrl',
+      resolve: {
+        partTypes: ['PartTypes', function(PartTypes) {
+          return PartTypes.getPromise();
+        }]
+      }
+    });
+    $routeProvider.when('/part/:id/form', {
+      templateUrl: 'views/part/PartForm.html',
+      controller: 'PartFormCtrl'
+    });
+    $routeProvider.when('/part/:id/interchange/search', {
+      templateUrl: 'views/part/interchange/PartInterchangeSearch.html',
+      controller: 'PartInterchangeSearchCtrl'
+    });
+    $routeProvider.when('/part/Kit/:id/component/search', {
+      templateUrl: '../views/part/KitComponentSearch.html',
+      controller: 'KitComponentSearchCtrl'
+    });
+    $routeProvider.when('/part/:id/bom/search', {
+      templateUrl: 'views/part/bom/PartBomSearch.html',
+      controller: 'PartBomSearchCtrl'
+    });
+    $routeProvider.when('/part/:id/bom/:bomId/search', {
+      templateUrl: 'views/part/bom/BomAlternateSearch.html',
+      controller: 'BomAlternateSearchCtrl'
+    });
+    $routeProvider.when('/part/:id/ancestors', {
+      templateUrl: 'views/part/PartAncestors.html',
+      controller: 'PartAncestorsCtrl'
+    });
+    $routeProvider.when('/part/:id', {
+      templateUrl: 'views/part/PartDetail.html',
+      controller: 'PartDetailCtrl'
+    });
+    $routeProvider.when('/part/:id/sales_notes', {
+      templateUrl: 'views/part/sales_note/list.html',
+      controller: 'PartSalesNoteListCtrl'
+    });
+    $routeProvider.when('/part/:id/sales_note/NewNote', {
+      templateUrl: 'views/part/sales_note/NewNote.html',
+      controller: 'PartSalesNewNoteCtrl'
+    });
+    $routeProvider.when('/part/:id/sales_note/ViewSalesNote', {
+      templateUrl: 'views/part/sales_note/ViewSalesNote.html',
+      controller: 'PartViewSalesNoteCtrl'
+    });
+
+
+    // Turbo Models
+    $routeProvider.when('/other/turboModels', {
+      templateUrl: 'views/other/TurboModels.html',
+      controller: 'TurboModelsCtrl'
+    });
 
 
 
-        // Users and groups
-        $routeProvider.when('/security/groups', {
-          templateUrl: 'views/security/groups.html',
-          controller: 'GroupsCtrl'
-        });
-        $routeProvider.when('/security/group/:id', {
-          templateUrl: 'views/security/group.html',
-          controller: 'GroupCtrl'
-        });
+    // Users and groups
+    $routeProvider.when('/security/groups', {
+      templateUrl: 'views/security/groups.html',
+      controller: 'GroupsCtrl'
+    });
+    $routeProvider.when('/security/group/:id', {
+      templateUrl: 'views/security/group.html',
+      controller: 'GroupCtrl'
+    });
 
-        $routeProvider.when('/security/users', {
-          templateUrl: 'views/security/users.html',
-          controller: 'UsersCtrl'
-        });
-        $routeProvider.when('/security/user/:id', {
-          templateUrl: 'views/security/user.html',
-          controller: 'UserCtrl'
-        });
+    $routeProvider.when('/security/users', {
+      templateUrl: 'views/security/users.html',
+      controller: 'UsersCtrl'
+    });
+    $routeProvider.when('/security/user/:id', {
+      templateUrl: 'views/security/user.html',
+      controller: 'UserCtrl'
+    });
 
-        // My Account
-        $routeProvider.when('/security/me', {
-          templateUrl: 'views/security/my-account.html',
-          controller: 'MyAccountCtrl'
-        });
+    // My Account
+    $routeProvider.when('/security/me', {
+      templateUrl: 'views/security/my-account.html',
+      controller: 'MyAccountCtrl'
+    });
 
-        // Password Reset
-        $routeProvider.when('/password/reset/:token', {
-          templateUrl: 'views/security/pwreset.html',
-          controller: 'LoginCtrl'
-        });
+    // Password Reset
+    $routeProvider.when('/password/reset/:token', {
+      templateUrl: 'views/security/pwreset.html',
+      controller: 'LoginCtrl'
+    });
 
-        // Default / Login
-        $routeProvider.when('/', {
-          templateUrl: 'views/security/login.html',
-          controller: 'LoginCtrl'
-        });
+    // Default / Login
+    $routeProvider.when('/', {
+      templateUrl: 'views/security/login.html',
+      controller: 'LoginCtrl'
+    });
 
-        $routeProvider.otherwise({
-          redirectTo: '/'
-        });
-      });
+    $routeProvider.otherwise({
+      redirectTo: '/'
+    });
+  });
--- metadata/yo/app/scripts/controllers/part/sales_note/NewNote.js
+++ metadata/yo/app/scripts/controllers/part/sales_note/NewNote.js
@@ -1,10 +1,5 @@
 'use strict';
 
 angular.module('ngMetaCrudApp')
-	.controller('PartSalesNewNoteCtrl', function($scope, $log, $q, $location,
-		$routeParams, ngTableParams, restService, Restangular, $dialogs, gToast) {
-		$scope.partId = $routeParams.id;
-
-		$scope.part = null;
-
+	.controller('PartSalesNewNoteCtrl', function() {
 	});
