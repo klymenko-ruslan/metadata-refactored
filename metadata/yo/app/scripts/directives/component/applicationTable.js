ba09ce98be6ad6274a012ab177afa21acc3d5caf
Dmytro Trunykov <dmitry.trunikov@gmail.com>
2015-11-24 08:03:54 +0200

    Added list of 'Applications'.

--- metadata/yo/app/scripts/directives/component/applicationTable.js
+++ metadata/yo/app/scripts/directives/component/applicationTable.js
@@ -1,25 +1,21 @@
 'use strict';
 
 angular.module('ngMetaCrudApp')
-    .directive('applicationTable', function ($log) {
+    .directive('applicationTable', function ($log,restService) {
       return {
-        scope: {
-          parts: '=',
-          key: '@'
-        },
+        scope: true,
         restrict: 'E',
         replace: false,
         transclude: true,
-        templateUrl: '/views/component/applicationTable.html'//,
+        templateUrl: '/views/component/applicationTable.html',
         controller: function($scope) {
-          // Applications.
           restService.findApplications($scope.partId).then(
             function (applications) {
-                $scope.applications = applications;
+              $scope.applications = applications;
             },
             function (errorResponse) {
-                $log.log("Could not get part's applications", errorResponse);
-                restService.error("Could not get part's applications", errorResponse);
+              $log.log("Could not get part's applications", errorResponse);
+              restService.error("Could not get part's applications", errorResponse);
             }
           );
         }
