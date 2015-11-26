f8a551dd8d110374c673c75b81ee55e99a47a51d
trunikov@home <dmitry.trunikov@gmail.com>
2015-11-26 22:53:12 +0200

    Intermediate commit.

--- metadata/src/main/java/com/turbointernational/metadata/domain/part/types/TurboCarModelEngineYear.java
+++ metadata/src/main/java/com/turbointernational/metadata/domain/part/types/TurboCarModelEngineYear.java
@@ -1,15 +1,52 @@
 package com.turbointernational.metadata.domain.part.types;
 
-
 import org.springframework.beans.factory.annotation.Configurable;
 
-import javax.persistence.Cacheable;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.io.Serializable;
 
-//@Cacheable
-//@Configurable
-//@Entity
-//@Table(name="turbo_car_model_engine_year")
-public class TurboCarModelEngineYear {
+@Cacheable
+@Configurable
+@Entity
+@Table(name="turbo_car_model_engine_year")
+public class TurboCarModelEngineYear implements Serializable {
+
+    @Id
+    @Column(name = "part_id", nullable = false)
+    private Long partId;
+
+    @Id
+    @Column(name = "car_model_engine_year_id", nullable = false)
+    private Long carModelEngineYearId;
+
+    @Column(name = "import_pk", nullable = true)
+    private Long importPk;
+
+    public TurboCarModelEngineYear() {
+    }
+
+    public Long getPartId() {
+        return partId;
+    }
+
+    public void setPartId(Long partId) {
+        this.partId = partId;
+    }
+
+    public Long getImportPk() {
+        return importPk;
+    }
+
+    public void setImportPk(Long importPk) {
+        this.importPk = importPk;
+    }
+
+    public Long getCarModelEngineYearId() {
+        return carModelEngineYearId;
+    }
+
+    public void setCarModelEngineYearId(Long carModelEngineYearId) {
+        this.carModelEngineYearId = carModelEngineYearId;
+    }
+
 }
--- metadata/yo/app/scripts/controllers/application/ApplicationDetail.js
+++ metadata/yo/app/scripts/controllers/application/ApplicationDetail.js
@@ -3,7 +3,7 @@
 angular.module('ngMetaCrudApp')
   .controller('ApplicationDetailCtrl', function ($scope, $routeParams, restService) {
     $scope.applicationId = $routeParams.id;
-    $scope.part = restService.findApplication($scope.applicationId)
+    restService.findApplication($scope.applicationId)
       .then(function (application) {
         $scope.application = application;
       }, function (errorResponse) {
