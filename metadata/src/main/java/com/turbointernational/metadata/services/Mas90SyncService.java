84325210ed55ff2c1645430c109262ebf848214a
trunikov@home <dmitry.trunikov@gmail.com>
2016-01-14 09:46:21 +0200

    Intermediate commit before fix of #562.

--- metadata/src/main/java/com/turbointernational/metadata/services/Mas90SyncService.java
+++ metadata/src/main/java/com/turbointernational/metadata/services/Mas90SyncService.java
@@ -1,6 +1,9 @@
 package com.turbointernational.metadata.services;
 
+import com.turbointernational.metadata.domain.other.Mas90Sync;
 import com.turbointernational.metadata.domain.other.Mas90SyncDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -8,6 +11,8 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
+import java.sql.Timestamp;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Synchronization service between Mas90 and 'metadata' database.
@@ -17,27 +22,46 @@ import javax.sql.DataSource;
 @Service
 public class Mas90SyncService {
 
-    public static class SyncProcessStatus {
+    private final Logger log = LoggerFactory.getLogger(Mas90SyncService.class);
 
+    public static class SyncProcessStatus {
+        final Mas90Sync record;
+        final int totalSteps;
+
+        AtomicInteger currentStep;
+
+        SyncProcessStatus(Mas90Sync record, int totalSteps) {
+            this.record = record;
+            this.totalSteps = totalSteps;
+            currentStep.set(0);
+        }
     }
 
     private class SyncProcess extends Thread {
 
-        private boolean stopped;
+        private final SyncProcessStatus status;
 
-        private final int numInterations;
-        private final long iterationPeriod;
-
-        SyncProcess(int numIterations, long iterationPeriod) {
-            this.numInterations = numIterations;
-            this.iterationPeriod = iterationPeriod;
+        SyncProcess(SyncProcessStatus status) {
+            this.status = status;
         }
 
         @Override
         public void run() {
-            for (int i = 0; !stopped && i < numInterations; i++) {
-
+            for (int i = 0; i < status.totalSteps; i++) {
+                synchronized (status) {
+                    if (status.record.getStatus() == Mas90Sync.Status.CANCELLED) {
+                        break;
+                    }
+                }
+                log.info("Synchronization: {}", i);
+                try {
+                    status.currentStep.incrementAndGet();
+                    Thread.sleep(1000L);
+                } catch(InterruptedException e) {
+                    log.warn("Unexpected interruption.", e);
+                }
             }
+            log.info("Synchronization process finished with status: {}", status.record.getStatus());
         }
 
     }
@@ -51,6 +75,8 @@ public class Mas90SyncService {
     @Autowired
     private Mas90SyncDao mas90SyncDao;
 
+    private SyncProcess syncProcess = null;
+
     @PostConstruct
     public void init() {
         mssqldb = new JdbcTemplate(dataSourceMas90);
@@ -61,6 +87,17 @@ public class Mas90SyncService {
     }
 
     public void start() {
+        if (syncProcess != null && syncProcess.isAlive()) {
+            throw new IllegalStateException("New 'sync.process' can't be started because exists other process.");
+        }
+        long now = System.currentTimeMillis();
+        Mas90Sync record = new Mas90Sync();
+        record.setStarted(new Timestamp(now));
+        record.setInserted(0L);
+        record.setUpdated(0L);
+        record.setToProcess(0L);
+        record.setStatus(Mas90Sync.Status.IN_PROGRESS);
+        mas90SyncDao.persist(record);
 
     }
 
