0646040bb149d503b14756aadd237c51fc2a685e
trunikov@home <dmitry.trunikov@gmail.com>
2016-08-11 22:39:25 +0300

    intermediate commit.

--- metadata/mockup/js/script.js
+++ metadata/mockup/js/script.js
@@ -1,3 +1,21 @@
 "use strict";
 
-angular.module("mockApp", ["ngRoute", "ngTable", "ui.bootstrap", "restangular", "dialogs.main", "gToast" ]);
+angular.module("mockApp", ["ngRoute", "ngTable", "ui.bootstrap",
+        "restangular", /*"dialogs.main",*/ "gToast" ]);
+
+
+
+angular.module("mockApp")
+  .directive("partSearch", ["$log", function($log) {
+    return {
+      restrict: "E",
+      replace: true,
+      templateUrl: "views/PartSearch.html",
+      transclude: true,
+      controller: ["$parse", "$sce", "$log", "$q", "$location", "$scope", "ngTableParams",
+        function($parse, $sce, $log, $q, $location, $scope, ngTableParams) {
+      }]
+    };
+  }]);
+
+ 
