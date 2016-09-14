f9df0e3712d3e09095792fd62185ae69a4cfd1b8
Dmytro Trunykov <dmytro.trunykov@zorallabs.com>
2016-09-14 10:39:44 +0300

    Improved enable/disable state update fro 'Pick' button.

--- metadata/yo/app/scripts/controllers/part/bom/ParentBomSearch.js
+++ metadata/yo/app/scripts/controllers/part/bom/ParentBomSearch.js
@@ -5,12 +5,21 @@ angular.module("ngMetaCrudApp").controller("ParentBomSearchCtrl", [
   function ($log, $scope, ngTableParams, dialogs, gToast, restService, BOM, utils, part, partTypes, parents) {
 
     $scope.part = part;
-    $scope.partTypes = partTypes;
+    //$scope.partTypes = partTypes;
     $scope.restService = restService;
 
     var pickedParts = [];
-    var pickedPartIds = {
+    var pickedPartIds = {};
+    var parentPartsIds = null;
+
+    function updateParentPartsIds() {
+      parentPartsIds = {};
+      _.each(parents, function(bi) {
+        parentPartsIds[bi.parent.id] = true;
+      });
     };
+
+    updateParentPartsIds();
 
     $scope.bomTableParams = new ngTableParams({
       page: 1,
@@ -30,13 +39,14 @@ angular.module("ngMetaCrudApp").controller("ParentBomSearchCtrl", [
       }
     );
 
-    $scope.bttnSaveDisabled = function() {
+    $scope.isBttnSaveDisabled = function() {
       return pickedParts.length === 0 || restService.status.bomRebuilding;
     };
 
-    $scope.bttnPickDisabled = function(p) {
+    $scope.isBttnPickDisabled = function(p) {
       return p === undefined || $scope.part.manufacturer.id != p.manufacturer.id ||
-        $scope.part.id == p.id || pickedPartIds[p.id];
+        $scope.part.id == p.id || pickedPartIds[p.id] || parentPartsIds[p.id] ||
+        restService.status.bomRebuilding;
     };
 
     $scope.save = function() {
@@ -53,6 +63,7 @@ angular.module("ngMetaCrudApp").controller("ParentBomSearchCtrl", [
           _.each(response.parents, function(b) {
             parents.push(b);
           });
+          updateParentPartsIds();
           $scope.bomTableParams.reload();
           _.each(pickedParts, function(p) {
             delete pickedPartIds[p.id];
