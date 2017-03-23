f73d76e225f2ad661f900056b66c5f3b3e7f076b
Dmytro Trunykov <dmytro.trunykov@zorallabs.com>
2017-03-23 10:10:47 +0200

    project clean up.

--- metadata/alter.sql
+++
@@ -1,4 +0,0 @@
-insert into role(id, name, display) values(32, 'ROLE_MANUFACTURER_CRUD', 'CRUD operations on manufacturers.');
-insert into group_role(group_id, role_id) values(3 , 32);
-
-alter table manfr add column not_external tinyint(1) not null default 0;
