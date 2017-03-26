2d0b2de691c17645aac6b15f97382feba39b781c
Dmitry Trunikov <dmytro.trunykov@zorallabs.com>
2017-03-26 23:21:33 +0300

    nc

--- metadata/src/main/java/com/turbointernational/metadata/dao/GroupDao.java
+++ metadata/src/main/java/com/turbointernational/metadata/dao/GroupDao.java
@@ -1,9 +1,12 @@
 package com.turbointernational.metadata.dao;
 
+import static javax.persistence.criteria.JoinType.LEFT;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.Tuple;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -11,11 +14,14 @@ import javax.persistence.criteria.Order;
 import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import javax.persistence.criteria.SetJoin;
 
 import org.springframework.stereotype.Repository;
 
 import com.turbointernational.metadata.entity.Group;
 import com.turbointernational.metadata.entity.Group_;
+import com.turbointernational.metadata.entity.User;
+import com.turbointernational.metadata.entity.User_;
 import com.turbointernational.metadata.web.dto.Page;
 
 /**
@@ -23,6 +29,9 @@ import com.turbointernational.metadata.web.dto.Page;
  */
 @Repository
 public class GroupDao extends AbstractDao<Group> {
+
+    public final static String ALIAS_GROUP = "group";
+    public final static String ALIAS_MEMBER = "member";
 
     public GroupDao() {
         super(Group.class);
@@ -37,20 +46,25 @@ public class GroupDao extends AbstractDao<Group> {
      * @param limit
      * @return
      */
-    public Page<Group> filter(Optional<String> fltrName, Optional<String> fltrRole, Optional<Boolean> fltrIsMemeber,
-            Optional<String> sortProperty, Optional<String> sortOrder, Optional<Integer> offset,
-            Optional<Integer> limit) {
+    public Page<Tuple> filter(Long userId, Optional<String> fltrName, Optional<String> fltrRole,
+            Optional<Boolean> fltrIsMemeber, Optional<String> sortProperty, Optional<String> sortOrder,
+            Optional<Integer> offset, Optional<Integer> limit) {
 
         // select /*distinct*/ g.id, g.name, ug.user_id is not null
         // from
-        //   groups as g left outer join user_group as ug on g.id = ug.group_id and ug.user_id = 4
+        // groups as g left outer join user_group as ug on g.id = ug.group_id
+        // and ug.user_id = 4
         // where
-        //   exists(select 1 from group_role as gr join role as r on gr.role_id=r.id where lower(r.name) like '%rb%')
+        // exists(select 1 from group_role as gr join role as r on
+        // gr.role_id=r.id where lower(r.name) like '%rb%')
         // order by 2
 
         CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<Group> cqg = cb.createQuery(Group.class);
+        CriteriaQuery<Tuple> cqg = cb.createTupleQuery();
         Root<Group> grpRoot = cqg.from(Group.class);
+        SetJoin<Group, User> usrRoot = grpRoot.join(Group_.users, LEFT);
+        usrRoot.on(cb.equal(usrRoot.get(User_.id), userId));
+        cqg.select(cb.tuple(grpRoot.alias(ALIAS_GROUP), cb.isNotNull(usrRoot).alias(ALIAS_MEMBER)));
         Path<String> colName = grpRoot.get(Group_.name);
         List<Predicate> lstWherePredicates = new ArrayList<>(3);
         fltrName.ifPresent(s -> cb.equal(cb.lower(colName), "%" + s.toLowerCase() + "%"));
@@ -72,10 +86,10 @@ public class GroupDao extends AbstractDao<Group> {
             }
             order.ifPresent(ord -> cqg.orderBy(ord));
         });
-        TypedQuery<Group> tq = em.createQuery(cqg);
+        TypedQuery<Tuple> tq = em.createQuery(cqg);
         offset.ifPresent(off -> tq.setFirstResult(off));
-        limit.ifPresent(lmt -> tq.setMaxResults(lmt)); 
-        List<Group> recs = tq.getResultList();
+        limit.ifPresent(lmt -> tq.setMaxResults(lmt));
+        List<Tuple> recs = tq.getResultList();
         CriteriaQuery<Long> cq = cb.createQuery(Long.class);
         cq.select(cb.count(grpRoot));
         cq.where(arrWherePredicates);
