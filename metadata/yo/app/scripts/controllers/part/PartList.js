5ee246d429dd26eae4d10ec673a00e48313571d5
Dmitry Trunikov <dmytro.trunykov@zorallabs.com>
2017-04-04 21:29:19 +0300

    nc

--- metadata/yo/app/scripts/controllers/part/PartList.js
+++ metadata/yo/app/scripts/controllers/part/PartList.js
@@ -10,9 +10,37 @@ angular.module("ngMetaCrudApp")
         "controller": "PartCreateModalCtrl"
       });
     };
+
     $scope.partTypes = partTypes;
     $scope.critDimsByPartTypes = critDimsByPartTypes;
     $scope.critDimEnumVals = critDimEnumVals;
+
+    $scope.editor1 = "Hello world xxx2!";
+    $scope.markdownEditorOpts = {
+      iconlibrary: "fa",
+      addExtraButtons: true,
+      resize: "vertical",
+      fullscreen: {enable: false},
+      hiddenButtons: "Preview",
+      dropZoneOptions: {
+        //url: "http://localhost:8080/metadata/file/post",
+        maxFilesize:20,
+        createImageThumbnails: false,
+        maxFiles: 100,
+        parallelUploads: 1,
+        previewsContainer: "#descriptionUploads",
+        previewTemplate: "<li><div class='dz-preview'><div class='dz-details'><span class='dz-filename' data-dz-name></span>, <span class='dz-size' data-dz-size></span><button class='btn btn-danger btn-xs' style='margin-left:4px' data-dz-remove><i class='fa fa-trash-o'></i> Remove</button><span style='padding-left:4px;' data-dz-errormessage></span></div></div></li>",
+        autoProcessQueue: false,
+        init: function() {
+          this.on("addedfile", function(file) {
+            alert("Added file: " + file.name + ", " + file.size + ", " + file.type);
+          });
+          this.on("removedfile", function(file) {
+            alert("Removed file: " + file.name + ", " + file.size + ", " + file.type);
+          });
+        }
+      }
+    };
 
   }
 ])
