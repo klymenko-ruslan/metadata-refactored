c8732d65b2dbd324d1748cde7c678ec096bdd558
Dmytro Trunykov <dmytro.trunykov@zorallabs.com>
2017-05-25 16:10:24 +0300

    intermediate commit

---
+++ metadata/yo/test/e2e/runtests.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim: set fileencoding=utf-8 :
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
+"""The script runs e2e tests."""
+
+import argparse
+import http.client
+import shutil
+import subprocess
+import sys
+import re
+
+print('Checking of prerequisites.')
+
+if shutil.which('protractor') is None:
+    print('Executable \'protractor\' not found. '
+          'Is it (http://www.protractortest.org) installed?', file=sys.stderr)
+    sys.exit(1)
+
+jpsout = subprocess.getoutput('jps')
+
+if re.search(r'selenium-server-standalone-[\d\.]+.jar', jpsout) is None:
+    print('Running instance of a webdriver-manager not found. '
+          'Please run: \'webdriver-manager start\' in a separate window.',
+          file=sys.stderr)
+    sys.exit(1)
+
+if re.search(r'Elasticsearch', jpsout) is None:
+    print('Running instance of an Elasticsearch not found.',
+          file=sys.stderr)
+    sys.exit(1)
+
+if re.search(r'metadata-e2e.jar', jpsout) is None:
+    print('Running instance of \'metadata\' webapp not found. '
+          'Please run \'metadata.py\'.', file=sys.stderr)
+    sys.exit(1)
+
+print('Indexing of all documetns.')
+
+try:
+    httpconn = http.client.HTTPConnection('localhost', 8080, timeout=60)
+    httpconn.request('POST', '/metadata/search/indexall')
+    response = httpconn.getresponse()
+except http.client.HTTPException as e:
+    print('Starting of the indexing job failed. Server response: {}'.format(e))
+    sys.exit(1)
+
+if response.status != http.client.OK:
+    print('The indexing job failed: {}')
+    sys.exit(1)
+print('The indexing has been finished.')
+
+subprocess.call('protractor ./protractor.conf.js', shell=True)
