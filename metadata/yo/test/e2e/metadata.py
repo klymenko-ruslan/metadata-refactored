61a27711377fe8aaa9ab4883face7bb2475094d0
Dmitry Trunikov <dmytro.trunykov@zorallabs.com>
2017-05-25 20:43:13 +0300

    intermediate commit

--- metadata/yo/test/e2e/metadata.py
+++ metadata/yo/test/e2e/metadata.py
@@ -15,16 +15,24 @@ calculate real pathes to targets. The calculation is done relative to
 a location of this script, so don't move this script in other dirs.
 """
 
+import argparse
 import os
 import re
 import shutil
 import subprocess
 import sys
 
+parser = argparse.ArgumentParser(description='Build and runs the \'metadata\' '
+                                 'webapp for e2e testing.')
+parser.add_argument('--java-extra-opts', default='',
+                    help='Extra options to run \'java\'.')
+args = parser.parse_args()
+
 
 def check_jdk():
     """
     Check presence of Java in the system and its version.
+
     If this check fails then script is finished.
     Returns full filename to the checked 'java' executable.
     """
@@ -56,6 +64,7 @@ def check_jdk():
 def check_maven():
     """
     Check presence of Apache Maven in the system and its version.
+
     If this check fails then script is finished.
     Returns full filename to the checked Apache Maven executable.
     """
@@ -98,11 +107,12 @@ if not os.path.exists(pomfilename):
 cmdmvn = ('{mvn} -f {pom} -DskipTests -Dyo.test.skip=true -DbuildNumber=e2e '
           'clean package'.format(mvn=mvnfname, pom=pomfilename))
 print('Building of the \'metadata\' webapp.\n')
-retcode = subprocess.call(cmdmvn, shell=True)
-if retcode != 0:
-    print('Building of the \'metadata\' webapp failed with return code: {}'
-          .format(retcode), file=sys.stderr)
-    sys.exit(1)
+
+# retcode = subprocess.call(cmdmvn, shell=True)
+# if retcode != 0:
+#     print('Building of the \'metadata\' webapp failed with return code: {}'
+#           .format(retcode), file=sys.stderr)
+#     sys.exit(1)
 print('Prepare environment to start the webapp.')
 # The variable 'vardir' below is a root for various file storages
 # in the webapp (images, attachments etc.).
@@ -115,16 +125,16 @@ os.makedirs(images_originals_dir)
 images_resized_dir = os.path.join(vardir, 'metadata', 'product_images',
                                   'resized')
 os.makedirs(images_resized_dir)
-attachments_salesnotes_dir=os.path.join(vardir, 'metadata', 'salesNote',
-                                        'attachments')
+attachments_salesnotes_dir = os.path.join(vardir, 'metadata', 'salesNote',
+                                          'attachments')
 os.makedirs(attachments_salesnotes_dir)
-changelog_sources_dir=os.path.join(vardir, 'metadata', 'changelog', 'sources',
-                                   'attachments')
+changelog_sources_dir = os.path.join(vardir, 'metadata', 'changelog',
+                                     'sources', 'attachments')
 os.makedirs(changelog_sources_dir)
-changelog_source_lnk_dscr_attch_dir=os.path.join(vardir, 'metadata',
-                                                 'changelog', 'sources',
-                                                 'link', 'description',
-                                                 'attachments')
+changelog_source_lnk_dscr_attch_dir = os.path.join(vardir, 'metadata',
+                                                   'changelog', 'sources',
+                                                   'link', 'description',
+                                                   'attachments')
 os.makedirs(changelog_source_lnk_dscr_attch_dir)
 print('Starting the built webapp.')
 jarfilename = os.path.join(scriptdir, '..', '..', '..', 'target',
@@ -134,12 +144,14 @@ cmdjava = ('{java} '
            '-Dimages.resized=file:{product_images_resized} '
            '-Dchangelog.sources.dir=file:{changelog_sources_dir} '
            '-Dchangelog.source.link.description.attachments.dir=file:{cldscr} '
+           '{extraopts} '
            '-jar {metadatajar} --spring.profiles.active=e2e').format(
                java=javafname,
                product_images_originals=images_originals_dir,
                product_images_resized=images_resized_dir,
                changelog_sources_dir=changelog_sources_dir,
                cldscr=changelog_source_lnk_dscr_attch_dir,
-               metadatajar=jarfilename
+               metadatajar=jarfilename,
+               extraopts=args.java_extra_opts
            )
 subprocess.call(cmdjava, shell=True)
