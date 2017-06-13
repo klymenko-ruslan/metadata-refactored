409d5d5d16da629d3479ec1cf33c9dcaf0883583
Dmytro Trunykov <dmytro.trunykov@zorallabs.com>
2017-06-13 11:58:40 +0300

    e2e - finished critical dimensions enums

--- metadata/yo/test/e2e/spec/critdimsenums.js
+++ metadata/yo/test/e2e/spec/critdimsenums.js
@@ -99,7 +99,7 @@ describe('Critical dimensions enumerations:', function() {
         expect(bttnCancel.isEnabled()).toBeTruthy();
       });
 
-      it('should clos the dialog when button \'Cancel\' is clicked',
+      it('should close the dialog when button \'Cancel\' is clicked',
         function() {
           expect(dlgNewEnum.isDisplayed()).toBeTruthy();
           bttnCancel.click();
@@ -150,7 +150,7 @@ describe('Critical dimensions enumerations:', function() {
         // deletion of the enum.
         bttnDeleteFoo.click();
         bttnConfirmDelete.click();
-        // The confirmation dialog should disappear and enum item
+        // The confirmation dialog should disappear and a row with enum
         // is removed from the table.
         expect(dlgDelEnum.isDisplayed()).toBeFalsy();
         expect(firstCell.getText()).not.toBe('foo');
@@ -172,7 +172,7 @@ describe('Critical dimensions enumerations:', function() {
         bttnModifyEnum.click();
       });
 
-      it('should have initial state',
+      it('should have an initial state',
         function() {
           expect(firstCellTxt.isDisplayed()).toBeFalsy();
           expect(firstCellInput.isDisplayed()).toBeTruthy();
@@ -285,7 +285,207 @@ describe('Critical dimensions enumerations:', function() {
       expect(bttnCancelEnumItem.isDisplayed()).toBeFalsy();
       expect(bttnUndoEnumItem.isPresent()).toBeTruthy();
       expect(bttnUndoEnumItem.isDisplayed()).toBeFalsy();
-});
+    });
+
+    describe('create:', function() {
+
+      var dlgNewEnumItem, dlgDelEnumItem, inptEnumItemName, bttnCreate,
+        bttnCancel;
+
+      beforeAll(function() {
+        dlgNewEnumItem = element(by.id('addEnumItmDlg'));
+        dlgDelEnumItem = element(by.id('delEnumItmDlg'));
+        inptEnumItemName = dlgNewEnumItem.element(by.id('addEnumItmDlgCtrlName'));
+        bttnCreate = dlgNewEnumItem.element(by.partialButtonText('Create'));
+        bttnCancel = dlgNewEnumItem.element(by.partialButtonText('Cancel'));
+      });
+
+      beforeEach(function() {
+        bttnAddEnumItem.click();
+      });
+
+      it('should have an initial state', function() {
+        expect(dlgNewEnumItem.isPresent()).toBeTruthy();
+        // We expect in the line below that dialog to create
+        // a new enumeration item is displayed because it is opened in
+        // the function beforeEach() above.
+        expect(dlgNewEnumItem.isDisplayed()).toBeTruthy();
+        expect(dlgDelEnumItem.isPresent()).toBeTruthy();
+        expect(dlgDelEnumItem.isDisplayed()).toBeFalsy();
+        expect(bttnCreate.isPresent()).toBeTruthy();
+        expect(bttnCreate.isDisplayed()).toBeTruthy();
+        expect(bttnCreate.isEnabled()).toBeFalsy();
+        expect(bttnCancel.isPresent()).toBeTruthy();
+        expect(bttnCancel.isDisplayed()).toBeTruthy();
+        expect(bttnCancel.isEnabled()).toBeTruthy();
+      });
+
+      it('should close the dialog when button \'Cancel\' is clicked',
+        function() {
+          expect(dlgNewEnumItem.isDisplayed()).toBeTruthy();
+          bttnCancel.click();
+          expect(dlgNewEnumItem.isDisplayed()).toBeFalsy();
+        }
+      );
+
+      it('should not allow to create a new enum item with non-unique name',
+        function() {
+          inptEnumItemName.sendKeys('YES');
+          expect(bttnCreate.isEnabled()).toBeFalsy();
+        }
+      );
+
+      it('should allow to create a new enum item and then delete it',
+        function() {
+          // Create a new enum.
+          inptEnumItemName.sendKeys('foo');
+          expect(bttnCreate.isEnabled()).toBeTruthy();
+          bttnCreate.click();
+          expect(dlgNewEnumItem.isDisplayed()).toBeFalsy();
+          // Check that enum item is displayed in the table.
+          var lastRowEnumItem = rowsEnumItems.last();
+          var firstCell = lastRowEnumItem.all(by.tagName('td'))
+            .first().all(by.tagName('span')).first();
+          expect(firstCell.isPresent());
+          expect(firstCell.isDisplayed());
+          expect(firstCell.getText()).toBe('foo');
+          // Delete the enum.
+          var bttnDeleteFoo = lastRowEnumItem.all(by.tagName('td')).last()
+            .element(by.tiButton('Delete'));
+          expect(bttnDeleteFoo.isPresent()).toBeTruthy();
+          expect(bttnDeleteFoo.isDisplayed()).toBeTruthy();
+          expect(bttnDeleteFoo.isEnabled()).toBeTruthy();
+          bttnDeleteFoo.click();
+          expect(dlgDelEnumItem.isDisplayed()).toBeTruthy();
+          var bttnConfirmDelete = dlgDelEnumItem.element(by.tiButton('Delete'));
+          expect(bttnConfirmDelete.isPresent()).toBeTruthy();
+          expect(bttnConfirmDelete.isDisplayed()).toBeTruthy();
+          expect(bttnConfirmDelete.isEnabled()).toBeTruthy();
+          var bttnCancelDelete = dlgDelEnumItem.element(by.tiButton('Cancel'));
+          expect(bttnCancelDelete.isPresent()).toBeTruthy();
+          expect(bttnCancelDelete.isDisplayed()).toBeTruthy();
+          expect(bttnCancelDelete.isEnabled()).toBeTruthy();
+          // Check that button 'Cancel' closes the dialog.
+          bttnCancelDelete.click();
+          expect(dlgDelEnumItem.isDisplayed()).toBeFalsy();
+          // Press the button 'Delete' in the table again and confirm
+          // deletion of the enum item.
+          bttnDeleteFoo.click();
+          bttnConfirmDelete.click();
+          // The confirmation dialog should disappear and a row with enum item
+          // is removed from the table.
+          expect(dlgDelEnumItem.isDisplayed()).toBeFalsy();
+          expect(firstCell.getText()).not.toBe('foo');
+        }
+      );
+
+    });
+
+    describe('edit:', function() {
+
+      var firstCellTxt, firstCellInput;
+
+      beforeAll(function() {
+        var firstCell = firstRowEnumItems.all(by.tagName('td')).first();
+        firstCellTxt = firstCell.all(by.tagName('span')).first();
+        firstCellInput = firstCell.all(by.tagName('input')).first();
+      });
+
+      beforeEach(function() {
+        bttnModifyEnumItem.click();
+      });
+
+      it('should have an initial state',
+        function() {
+          expect(firstCellTxt.isDisplayed()).toBeFalsy();
+          expect(firstCellInput.isDisplayed()).toBeTruthy();
+          expect(bttnModifyEnumItem.isDisplayed()).toBeFalsy();
+          expect(bttnDeleteEnumItem.isDisplayed()).toBeFalsy();
+          expect(bttnSaveEnumItem.isDisplayed()).toBeTruthy();
+          expect(bttnSaveEnumItem.isEnabled()).toBeFalsy();
+          expect(bttnUndoEnumItem.isDisplayed()).toBeTruthy();
+          expect(bttnUndoEnumItem.isEnabled()).toBeFalsy();
+          expect(bttnCancelEnumItem.isDisplayed()).toBeTruthy();
+          expect(bttnCancelEnumItem.isEnabled()).toBeTruthy();
+        }
+      );
+
+      it('should revert to view record mode ' +
+        'when button \'Cancel\' is clicked',
+        function() {
+          bttnCancelEnumItem.click();
+          expect(firstCellTxt.isDisplayed()).toBeTruthy();
+          expect(firstCellInput.isDisplayed()).toBeFalsy();
+          expect(bttnModifyEnum.isDisplayed()).toBeTruthy();
+          expect(bttnModifyEnum.isEnabled()).toBeTruthy();
+          expect(bttnDeleteEnum.isDisplayed()).toBeTruthy();
+          expect(bttnDeleteEnum.isEnabled()).toBeTruthy();
+          expect(bttnSaveEnum.isDisplayed()).toBeFalsy();
+          expect(bttnUndoEnum.isDisplayed()).toBeFalsy();
+          expect(bttnCancelEnum.isDisplayed()).toBeFalsy();
+        }
+      );
+
+      it('should allow to undo changes', function() {
+        expect(firstCellInput.evaluate('cdevModifying.val'))
+          .toBe('YES');
+        firstCellInput.sendKeys('foo');
+        expect(firstCellInput.evaluate('cdevModifying.val'))
+          .toBe('YESfoo');
+        expect(bttnSaveEnumItem.isEnabled()).toBeTruthy();
+        expect(bttnUndoEnumItem.isEnabled()).toBeTruthy();
+        bttnUndoEnumItem.click();
+        expect(firstCellInput.evaluate('cdevModifying.val'))
+          .toBe('YES');
+      });
+
+      it('should allow to save changes', function() {
+        // Do a modification.
+        expect(firstCellInput.evaluate('cdevModifying.val'))
+          .toBe('YES');
+        firstCellInput.sendKeys('foo');
+        expect(firstCellInput.evaluate('cdevModifying.val'))
+          .toBe('YESfoo');
+        expect(bttnSaveEnumItem.isEnabled()).toBeTruthy();
+        // Save the modification.
+        bttnSaveEnumItem.click();
+        // Check that modified value is displayed in the table row.
+        expect(firstCellTxt.isDisplayed()).toBeTruthy();
+        expect(firstCellInput.isDisplayed()).toBeFalsy();
+        expect(bttnModifyEnumItem.isDisplayed()).toBeTruthy();
+        expect(bttnModifyEnumItem.isEnabled()).toBeTruthy();
+        expect(bttnDeleteEnumItem.isDisplayed()).toBeTruthy();
+        expect(bttnDeleteEnumItem.isEnabled()).toBeTruthy();
+        expect(bttnSaveEnumItem.isDisplayed()).toBeFalsy();
+        expect(bttnUndoEnumItem.isDisplayed()).toBeFalsy();
+        expect(bttnCancelEnumItem.isDisplayed()).toBeFalsy();
+        expect(firstCellTxt.getText()).toBe('YESfoo');
+        // Do modification again to revert the old value.
+        bttnModifyEnumItem.click();
+        firstCellInput.clear();
+        firstCellInput.sendKeys('YES');
+        bttnSaveEnumItem.click();
+        expect(firstCellTxt.getText()).toBe('YES');
+      });
+
+      describe('validation:', function() {
+
+        it('should not allow to save modifications with empty', function() {
+          firstCellInput.clear();
+          expect(bttnSaveEnumItem.isEnabled()).toBeFalsy();
+        });
+
+        it('should not allow to save modifications with non-uniqe name',
+          function() {
+            firstCellInput.clear();
+            firstCellInput.sendKeys('NO');
+            expect(bttnSaveEnumItem.isEnabled()).toBeFalsy();
+          }
+        );
+
+      });
+
+    });
 
   });
 
