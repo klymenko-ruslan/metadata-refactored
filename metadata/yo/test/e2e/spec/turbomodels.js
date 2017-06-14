067242e1708b5da426054bff8d8c840913bebee7
Dmytro Trunykov <dmytro.trunykov@zorallabs.com>
2017-06-14 15:05:36 +0300

    nc

--- metadata/yo/test/e2e/spec/turbomodels.js
+++ metadata/yo/test/e2e/spec/turbomodels.js
@@ -65,15 +65,15 @@ describe('Turbo Models:', function() {
       expect(fltrTurboType.isPresent()).toBeTruthy();
       expect(fltrTurboType.isDisplayed()).toBeTruthy();
       expect(fltrTurboType.isEnabled()).toBeTruthy();
-      expect(elmTurboType.evaluate('turboModels.length')).toBeNull();
+      expect(elmTurboType.evaluate('turboTypes.length')).toBeNull();
       expect(bttnCreateType.isPresent()).toBeTruthy();
       expect(bttnCreateType.isDisplayed()).toBeTruthy();
       expect(bttnCreateType.isEnabled()).toBeTruthy();
       expect(bttnRenameType.isPresent()).toBeTruthy();
       expect(bttnRenameType.isDisplayed()).toBeTruthy();
       //expect(bttnRenameType.isEnabled()).toBeFalsy();
-      expect(bttnDeleteType .isPresent()).toBeTruthy();
-      expect(bttnDeleteType .isDisplayed()).toBeTruthy();
+      expect(bttnDeleteType.isPresent()).toBeTruthy();
+      expect(bttnDeleteType.isDisplayed()).toBeTruthy();
       //expect(bttnDeleteType .isEnabled()).toBeFalsy();
     });
 
@@ -123,7 +123,7 @@ describe('Turbo Models:', function() {
           by.partialButtonText('Cancel'));
         bttnCreate = dlgCreateTurboType.element(
           by.partialButtonText('Create'));
-        inputName = element(by.model('name'));
+        inputName = dlgCreateTurboType.element(by.model('name'));
       });
 
       beforeEach(function() {
@@ -325,6 +325,291 @@ describe('Turbo Models:', function() {
       );
 
     });
+
+  });
+
+  describe('turbo model:', function() {
+
+    var fltrTurboModel, bttnCreateModel, bttnRenameModel, bttnDeleteModel;
+
+    beforeAll(function() {
+      fltrTurboModel = element(by.model('turboModelFilter'));
+      bttnCreateModel = element(by.partialLinkText('Create Model'));
+      bttnRenameModel = element(by.partialLinkText('Rename Model'));
+      bttnDeleteModel = element(by.partialLinkText('Delete Model'));
+    });
+
+    it('should have an initial state', function() {
+      expect(fltrTurboModel.isPresent()).toBeTruthy();
+      expect(fltrTurboModel.isDisplayed()).toBeTruthy();
+      expect(fltrTurboModel.isEnabled()).toBeTruthy();
+      expect(elmTurboModel.evaluate('turboModels.length')).toBeNull();
+      expect(bttnCreateModel.isPresent()).toBeTruthy();
+      expect(bttnCreateModel.isDisplayed()).toBeTruthy();
+      expect(bttnCreateModel.isEnabled()).toBeTruthy();
+      expect(bttnRenameModel.isPresent()).toBeTruthy();
+      expect(bttnRenameModel.isDisplayed()).toBeTruthy();
+      //expect(bttnRenameModel.isEnabled()).toBeFalsy();
+      expect(bttnDeleteModel.isPresent()).toBeTruthy();
+      expect(bttnDeleteModel.isDisplayed()).toBeTruthy();
+      //expect(bttnDeleteModel.isEnabled()).toBeFalsy();
+    });
+
+    describe('filter by name:', function() {
+
+      var tmOpts;
+
+      beforeAll(function() {
+        tmOpts = elmTurboModel.all(by.tagName('option'));
+      });
+
+      beforeEach(function() {
+        browser._selectDropdownbyNum(elmManufacturer, 3); // Holset
+        browser._selectDropdownbyNum(elmTurboType, 1);    // H1C
+      });
+
+      it('should display only turbo models for the selected turbo type',
+        function() {
+          expect(elmTurboModel.evaluate('turboModels.length')).toBe(1);
+          expect(tmOpts.count()).toBe(2);
+        }
+      );
+
+      it('should filter by exact val', function() {
+        fltrTurboModel.sendKeys('H1C');
+        expect(tmOpts.count()).toBe(2); // first option is empty
+      });
+
+      it('should filter by partial val, case sensitively', function() {
+        fltrTurboModel.sendKeys('C');
+        expect(tmOpts.count()).toBe(2); // first option is empty
+      });
+
+      it('should filter by partial val, case insensitively', function() {
+        fltrTurboModel.sendKeys('c');
+        expect(tmOpts.count()).toBe(2); // first option is empty
+      });
+
+    });
+
+    describe('create:', function() {
+
+      var dlgCreateTurboModel, bttnCancel, bttnCreate, inputName;
+
+      beforeAll(function() {
+        dlgCreateTurboModel = element(by.css('.modal-dialog'));
+        bttnCancel = dlgCreateTurboModel.element(
+          by.partialButtonText('Cancel'));
+        bttnCreate = dlgCreateTurboModel.element(
+          by.partialButtonText('Create'));
+        inputName = dlgCreateTurboModel.element(by.model('name'));
+      });
+
+      beforeEach(function() {
+        browser._selectDropdownbyNum(elmManufacturer, 3); // Holset
+        browser._selectDropdownbyNum(elmTurboType, 1);    // H1C
+        bttnCreateModel.click();
+      });
+
+      it('should have an initial state', function() {
+        // Check that a dialog to create a new turbo model is displayed.
+        expect(dlgCreateTurboModel.isPresent()).toBeTruthy();
+        expect(dlgCreateTurboModel.isDisplayed()).toBeTruthy();
+        expect(inputName.isPresent()).toBeTruthy();
+        expect(inputName.isDisplayed()).toBeTruthy();
+        expect(inputName.isEnabled()).toBeTruthy();
+        expect(bttnCancel.isPresent()).toBeTruthy();
+        expect(bttnCancel.isDisplayed()).toBeTruthy();
+        expect(bttnCancel.isEnabled()).toBeTruthy();
+        expect(bttnCreate.isPresent()).toBeTruthy();
+        expect(bttnCreate.isDisplayed()).toBeTruthy();
+        expect(bttnCreate.isEnabled()).toBeTruthy();
+      });
+
+      it('should close the dilog when button \'Cancel\' is clicked',
+        function() {
+          bttnCancel.click();
+          expect(dlgCreateTurboModel.isPresent()).toBeFalsy();
+        }
+      );
+
+      it('should create a new turbo model', function() {
+        inputName.sendKeys('FOO');
+        expect(bttnCreate.isPresent()).toBeTruthy();
+        expect(bttnCreate.isDisplayed()).toBeTruthy();
+        expect(bttnCreate.isEnabled()).toBeTruthy();
+        bttnCreate.click();
+        // The dialog should be hidden.
+        browser.wait(EC.invisibilityOf(dlgCreateTurboModel), 1000,
+          'A dialog to create a new turbo model was not hidden.');
+        elmTurboType.evaluate('turboModels').then(function(turboModels) {
+          var found =  false;
+          for(var tm of turboModels) {
+            if (tm.name === 'FOO') {
+              found = true;
+              break;
+            }
+          }
+          expect(found).toBeTruthy();
+        });
+      });
+
+      // TODO: validators
+
+    });
+
+    describe('rename:', function() {
+
+      var dlgRenameTurboModel, bttnCancel, bttnRename, inputName;
+
+      beforeAll(function() {
+        dlgRenameTurboModel = element(by.css('.modal-dialog'));
+        bttnCancel = dlgRenameTurboModel.element(
+          by.partialButtonText('Cancel'));
+        bttnRename = dlgRenameTurboModel.element(
+          by.partialButtonText('Rename'));
+        inputName = dlgRenameTurboModel.element(by.model('name'));
+      });
+
+      beforeEach(function() {
+        browser._selectDropdownbyNum(elmManufacturer, 3); // Holset
+        browser._selectDropdownbyNum(elmTurboType, 1);    // H1C
+        browser._selectDropdownbyNum(elmTurboModel, 1);   // FOO
+        bttnRenameModel.click();
+      });
+
+      it('should close the dilog when button \'Cancel\' is clicked',
+        function() {
+          bttnCancel.click();
+          expect(dlgRenameTurboModel.isPresent()).toBeFalsy();
+        }
+      );
+
+      it('should have an initial state', function() {
+        // Check that a dialog to create a new turbo model is displayed.
+        expect(dlgRenameTurboModel.isPresent()).toBeTruthy();
+        expect(dlgRenameTurboModel.isDisplayed()).toBeTruthy();
+        expect(inputName.isPresent()).toBeTruthy();
+        expect(inputName.isDisplayed()).toBeTruthy();
+        expect(inputName.isEnabled()).toBeTruthy();
+        expect(bttnCancel.isPresent()).toBeTruthy();
+        expect(bttnCancel.isDisplayed()).toBeTruthy();
+        expect(bttnCancel.isEnabled()).toBeTruthy();
+        expect(bttnRename.isPresent()).toBeTruthy();
+        expect(bttnRename.isDisplayed()).toBeTruthy();
+        expect(bttnRename.isEnabled()).toBeTruthy();
+        expect(elmTurboType.evaluate('selection.turboModel.name')).toBe('FOO');
+      });
+
+      it('should rename a turbo model', function() {
+        inputName.clear(); // clear 'FOO'
+        inputName.sendKeys('FO');
+        bttnRename.click();
+        // The dialog should be hidden.
+        browser.wait(EC.invisibilityOf(dlgRenameTurboModel), 1000,
+          'A dialog to rename a turbo model was not hidden.');
+        elmTurboModel.evaluate('turboModels').then(function(turboModels) {
+          var found =  false;
+          for(var tm of turboModels) {
+            if (tm.name === 'FO') {
+              found = true;
+              break;
+            }
+          }
+          expect(found).toBeTruthy();
+        });
+      });
+
+      // TODO: validators
+
+    });
+
+    describe('delete:', function() {
+
+      var dlgDeleteTurboModel, bttnNo, bttnYes, dlgDeleteTurboModelFailure,
+        bttnClose;
+
+      beforeAll(function() {
+        dlgDeleteTurboModel = element(by.css('.modal-dialog'));
+        bttnNo = dlgDeleteTurboModel.element(
+          by.partialButtonText('No'));
+        bttnYes = dlgDeleteTurboModel.element(
+          by.partialButtonText('Yes'));
+        dlgDeleteTurboModelFailure = element(by.css('.modal-dialog'));
+        bttnClose = dlgDeleteTurboModelFailure.element(
+          by.partialButtonText('Close'));
+      });
+
+      beforeEach(function() {
+        browser._selectDropdownbyNum(elmManufacturer, 3); // Holset
+        browser._selectDropdownbyNum(elmTurboType, 1);    // H1C
+      });
+
+      it('should close the dilog when button \'No\' is clicked',
+        function() {
+          browser._selectDropdownbyNum(elmTurboModel, 1); // FO
+          expect(bttnDeleteModel.isEnabled()).toBeTruthy();
+          bttnDeleteModel.click();
+          expect(dlgDeleteTurboModel.isDisplayed()).toBeTruthy();
+          bttnNo.click();
+          expect(dlgDeleteTurboModel.isPresent()).toBeFalsy();
+        }
+      );
+
+      it('should allow to delete a turbo type', function() {
+        browser._selectDropdownbyNum(elmTurboModel, 1); // FO
+        expect(elmTurboModel.evaluate('selection.turboModel.name')).toBe('FO');
+        expect(bttnDeleteModel.isEnabled()).toBeTruthy();
+        bttnDeleteModel.click();
+        expect(dlgDeleteTurboModel.isDisplayed()).toBeTruthy();
+        bttnYes.click();
+        // The dialog should be hidden.
+        browser.wait(EC.invisibilityOf(dlgDeleteTurboModel), 1000,
+          'A dialog to delete a turbo model was not hidden.');
+        elmTurboModel.evaluate('turboModels').then(function(turboModels) {
+          var found =  false;
+          for(var tm of turboModels) {
+            if (tm.name === 'FO') {
+              found = true;
+              break;
+            }
+          }
+          expect(found).toBeFalsy();
+        });
+      });
+
+      /*
+      it('should not allow to delete a turbo type which is in use',
+        function() {
+          browser._selectDropdownbyNum(elmTurboType, 1); // H1C
+          expect(elmTurboType.evaluate('selection.turboType.name')).toBe('H1C');
+          expect(bttnDeleteType.isEnabled()).toBeTruthy();
+          bttnDeleteType.click();
+          expect(dlgDeleteTurboType.isDisplayed()).toBeTruthy();
+          bttnYes.click();
+          // A dialog with message:
+          //   Turbo type must not be used for any parts or turbo models.
+          //   Check server log for details.
+          // should be displayed.
+          expect(dlgDeleteTurboTypeFailure.isDisplayed()).toBeTruthy();
+          bttnClose.click();
+          expect(dlgDeleteTurboTypeFailure.isPresent()).toBeFalsy();
+          elmTurboType.evaluate('turboTypes').then(function(turboTypes) {
+            var found =  false;
+            for(var tt of turboTypes) {
+              if (tt.name === 'H1C') {
+                found = true;
+                break;
+              }
+            }
+            expect(found).toBeTruthy();
+          });
+        }
+      );
+      */
+
+    });
+
   });
 
 });
