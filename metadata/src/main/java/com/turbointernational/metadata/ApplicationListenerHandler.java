cfe1ea63ed2c9e0425654a96257f3440ff770700
Dmytro Trunykov <dmytro.trunykov@zorallabs.com>
2017-06-29 11:38:34 +0300

    merging.

--- metadata/src/main/java/com/turbointernational/metadata/ApplicationListenerHandler.java
+++ metadata/src/main/java/com/turbointernational/metadata/ApplicationListenerHandler.java
@@ -1,7 +1,5 @@
 package com.turbointernational.metadata;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
@@ -19,7 +17,7 @@ import com.turbointernational.metadata.service.CriticalDimensionService;
 @Component
 public class ApplicationListenerHandler implements ApplicationListener<ApplicationReadyEvent> {
 
-    private static final Logger log = LoggerFactory.getLogger(ApplicationListenerHandler.class);
+    // private static final Logger log = LoggerFactory.getLogger(ApplicationListenerHandler.class);
 
     @Autowired
     private CriticalDimensionService criticalDimensionService;
--- metadata/src/main/java/com/turbointernational/metadata/web/controller/CarModelController.java
+++ metadata/src/main/java/com/turbointernational/metadata/web/controller/CarModelController.java
@@ -1,16 +1,5 @@
 package com.turbointernational.metadata.web.controller;
 
-import com.fasterxml.jackson.annotation.JsonView;
-import com.turbointernational.metadata.entity.CarModel;
-import com.turbointernational.metadata.dao.CarModelDao;
-import com.turbointernational.metadata.util.View;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
-
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -18,6 +7,21 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 import static org.springframework.web.bind.annotation.RequestMethod.PUT;
 
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import com.turbointernational.metadata.dao.CarModelDao;
+import com.turbointernational.metadata.entity.CarModel;
+import com.turbointernational.metadata.util.View;
 
 /**
  * Created by dmytro.trunykov@zorallabs.com on 2015-12015.
