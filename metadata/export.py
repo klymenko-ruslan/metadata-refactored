79f13cc87b0e678bdd10b0bab745f1bbf9f8bf91
Dmytro Trunykov <dmytro.trunykov@zorallabs.com>
2017-08-04 17:45:49 +0300

    intermediate commit

--- metadata/export.py
+++ metadata/export.py
@@ -5,9 +5,11 @@
 """Utility to export 'metadata' MySql database to a JSON."""
 
 import argparse
-import json
+# import json
+import simplejson as json
 import mysql.connector
 from collections import namedtuple
+from tqdm import tqdm
 
 parser = argparse.ArgumentParser(description='Utility to export some entities'
                                  'of the \'metadata\' MySql database to '
@@ -22,6 +24,8 @@ parser.add_argument('--db-username', default='metaserver',
                     help='Username for the database \'metadata\'.')
 parser.add_argument('--db-password', default='metaserver',
                     help='Password for the user \'metaserver\'')
+parser.add_argument('--out', default='metadata_arangodb.json',
+                    help='Filename for output of a result.')
 args = parser.parse_args()
 
 EntityMeta = namedtuple('EntityMeta', ['tablename', 'extra_columns'])
@@ -31,94 +35,84 @@ ENTITIES_DESCRIPTORS = {
     1: EntityMeta('turbo', 'turbo_model_id, cool_type_id, gasket_kit_id'),
     2: EntityMeta('cartridge', None),
     3: EntityMeta('kit', 'kit_type_id'),
-    4: EntityMeta('piston_ring', 'installedDiameterA, gapBInstalledDiameter, '
-                  'thicknessC, thicknessCTol, widthD, widthDUpperTol, '
-                  'widthDLowerTol, material, type, weight as cd_weight, '
-                  'diagram'),
-    5: EntityMeta('journal_bearing', 'maxOuterDiameter, minOuterDiameter, '
-                  'maxInnerDiameter, minInnerDiameter, size, length, '
-                  'lengthTol, feedHoleCount, feedHoleDiameter, freePinned, '
-                  'bearingsPerCartridge, oilFeed, rotation, material, '
-                  'centerDiameterOd, endConfiguration, boreConfiguration, '
-                  'weight as cd_weight, specialFeatures, '
-                  'specialFeaturesOnOff, brgSurfaces, diagram'),
-    6: EntityMeta('gasket', 'type, splitSinglePassage, shape, boltHoles, '
-                  'passageA, b, c, d, e, f, g, h, thickness, material, '
-                  'weight as cd_weight, diagram'),
+    4: EntityMeta('piston_ring', None),
+    5: EntityMeta('journal_bearing', None),
+    6: EntityMeta('gasket', None),
     8: EntityMeta('fast_wearing_component', None),
     9: EntityMeta('major_component', None),
-    10: EntityMeta('minor_component', None)
-    11: EntityMeta('compressor_wheel', 'rotation, flatbackSuperback, '
-                   'extendedTips, threadedBore, boreless, inducerDiameterA, '
-                   'inducerDiameterATol, exducerDiameterB, '
-                   'exducerDiameterBTol, tipLocation, tipLocationTol, '
-                   'tipHeightD, tipHeightDTol, platformHeightE, '
-                   'platformHeightTol, maxBoreDiameter, minBoreDiameter, '
-                   'bladeCount, platformThickness, platformThicknessTol, '
-                   'threadCallout, threadHand, platformDiameterF, '
-                   'platformDiameterFTol, overallHeightC, noseDiameterG, '
-                   'footDiameterH, bladeHeight, weight as cd_weight, diagram'),
-    12: EntityMeta('turbine_wheel', 'inducerDiaA, inducerDiaATol, '
-                   'exducerDiaB, exducerDiaBTol, tipHeightC, tipHeightCTol, '
-                   'maxJournalDiameterD, minJournalDiameterD, '
-                   'minStemDiameterE, maxStemDiameterE, stemLengthF, '
-                   'stemLengthFTol, platformHeightH, bladeHeight, '
-                   'threadLengthG, thread, threadHand, '
-                   'pistonRingGrooveMajorDiaI, pistonRingGrooveMajorDiaITol, '
-                   'pistonRingGrooveMinorDiaJ, pistonRingGrooveMinorDiaJTol, '
-                   'pistonRingGrooveWidthK, pistonRingGrooveWidthKTol, '
-                   'the2ndPistonRingGrooveMajorDia, '
-                   'the2ndPistonRingGrooveMajorDiaTol, '
-                   'the2ndPistonRingGrooveMinorDia, '
-                   'the2ndPistonRingGrooveMinorDiaTol, '
-                   'the2ndPistonRingGrooveWidth, '
-                   'the2ndPistonRingGrooveWidthTol, rotation, bladeCount, '
-                   'shroudType, journalType, extendedTips, '
-                   'weight as cd_weight, diagram'),
-    11: EntityMeta('', ''),
-    11: EntityMeta('', ''),
-    11: EntityMeta('', ''),
-    11: EntityMeta('', ''),
-    11: EntityMeta('', ''),
-    11: EntityMeta('', ''),
-    11: EntityMeta('', ''),
-    11: EntityMeta('', ''),
-    11: EntityMeta('', ''),
-    11: EntityMeta('', ''),
-    11: EntityMeta('', ''),
-    11: EntityMeta('', ''),
-    11: EntityMeta('', ''),
+    10: EntityMeta('minor_component', None),
+    11: EntityMeta('compressor_wheel', None),
+    12: EntityMeta('turbine_wheel', None),
+    13: EntityMeta('bearing_housing', None),
+    14: EntityMeta('backplate_sealplate', None),
+    15: EntityMeta('heatshield', None),
+    16: EntityMeta('nozzle_ring', None),
+    17: EntityMeta('o_ring', None),
+    18: EntityMeta('oil_deflector', None),
+    19: EntityMeta('clamp', None),
+    20: EntityMeta('thrust_part', None),            # it has invalid value
+    21: EntityMeta('misc_minor_component', None),   # it has invalid value
+    30: EntityMeta('actuator', None),
+    31: EntityMeta('compressor_cover', None),
+    32: EntityMeta('plug', None),
+    33: EntityMeta('turbine_housing', None),
+    34: EntityMeta('backplate', None),
+    35: EntityMeta('bolt_screw', None),
+    36: EntityMeta('fitting', None),
+    37: EntityMeta('journal_bearing_spacer', None),
+    38: EntityMeta('nut', None),
+    39: EntityMeta('pin', None),
+    40: EntityMeta('retaining_ring', None),
+    41: EntityMeta('seal_plate', None),
+    42: EntityMeta('spring', None),
+    43: EntityMeta('thrust_bearing', None),
+    44: EntityMeta('thrust_collar', None),
+    45: EntityMeta('thrust_spacer', None),
+    46: EntityMeta('thrust_washer', None),
+    47: EntityMeta('washer', None),
+    48: EntityMeta('carbon_seal', None),
+    49: EntityMeta('gasket_kit', None),
+    50: EntityMeta('misc', None),
+    51: EntityMeta('p', None),
+    52: EntityMeta('shroud', None)
 }
+
+
+def normalize_crit_dim_col(colname):
+    """
+    Normalize column name of a critical dimension.
+
+    Some columnt in critical diminsions can have the same name as in
+    a main table of part (e.g. column 'weight' has in the table 'part'
+    and in some critical dimensions). This function conver such columns
+    to a form 'e.<colname> as cd_<colname>'.
+    """
+    if colname in ['weight']:
+        colname = 'e.{colname} as cd_{colname}'.format(colname=colname)
+    return colname
 
 
 def parttype2query(ptid):
     """Return SQL query to extract a part entity for specified part type."""
     em = ENTITIES_DESCRIPTORS.get(ptid)
-    retval = '''
-    select
-    id, manfr_part_num, manfr_id, part_type_id, name,
-    description, inactive, legend_img_filename, dim_length,
-    dim_height, weight
-    '''
-    if em.extra_cols is not None:
-        retval = retval + ', ' + em.extra_cols + ' '
-    retval = (retval + 'from part p join ' + em.tablename +
-              ' e on p.id=e.part_id where p.id=%{id}s')
-
-PART_COLS = ('id, manfr_part_num, manfr_id, part_type_id, name, description, '
-             'inactive, legend_img_filename, dim_length, dim_height, weight')
-
-SQL_DICT = {
-    # Turbo
-    1:  'select ' + PART_COLS + ', turbo_model_id, cool_type_id, ' +
-        'gasket_kit_id from part p join turbo t on p.id=t.part_id ' +
-        'where p.id=%(id)s',
-    # Cartridge
-    2:  'select ' + PART_COLS + ' from part p join cartridge c '
-        'on p.id=c.part_id where p.id=%(id)s',
-    # Kit
-    3:  'select ' + PART_COLS + ' from part p join cartridge c '
-}
+    retval = ('select p.id as id, p.manfr_part_num as manfr_part_num, '
+              'p.manfr_id as manfr_id, p.part_type_id as part_type_id, '
+              'p.name as name, p.description as description, '
+              'p.inactive as inactive, '
+              'p.legend_img_filename as legend_img_filename, '
+              'p.dim_length as dim_length, p.dim_height as dim_height, '
+              'p.weight as weight')
+    if em.extra_columns is not None:
+        retval = retval + ', ' + em.extra_columns
+    crit_dims = crit_dims_map.get(ptid, None)
+    if crit_dims is not None:
+        retval = (retval + ', ' +
+                  ', '.join(map(lambda cd:
+                                normalize_crit_dim_col(cd['json_name']),
+                                crit_dims)))
+    retval = (retval + ' from part p left outer join ' + em.tablename +
+              ' e on p.id=e.part_id where p.id=%(id)s')
+    return retval
 
 
 def normalize_manfrid_in_tt(tt):
@@ -139,42 +133,125 @@ def normalize_ttid_in_tm(tm):
     return tm
 
 
+def normalize_part(p):
+    """Replace manfr_id and part_type_id by objects."""
+    manfr_id = p.pop('manfr_id')
+    m = mnfr_map.get(manfr_id)
+    p['manufacturer'] = m
+    ptid = p.pop('part_type_id')
+    pt = parttypes_map.get(ptid)
+    p['part_type'] = pt
+    return p
+
+
+def load_part_short(cur, pid):
+    """
+    Load a part.
+
+    The function loads some main part attributes only.
+    """
+    cur.execute('select id, manfr_part_num, manfr_id, part_type_id, name '
+                'from part where id=%(id)s', {'id': pid})
+    p = cur.fetchone()
+    normalize_part(p)
+    return p
+
+
+def load_boms(cur, pid):
+    """Load parts's BOMs."""
+    retval = []
+    cur.execute('select id, child_part_id, quantity from bom '
+                'where parent_part_id=%(ppid)s', {'ppid': pid})
+    for bom in cur.fetchall():
+        chldpid = bom.pop('child_part_id')
+        chldp = load_part_short(cur, chldpid)
+        bom['child'] = chldp
+        retval.append(bom)
+    return retval
+
+
+def load_interchanges(cur, pid):
+    """Load interchanges for a part."""
+    retval = []
+    cur.execute('select interchange_header_id from interchange_item '
+                'where part_id=%(pid)s', {'pid': pid})
+    row = cur.fetchone()
+    if row is not None:
+        hid = row['interchange_header_id']
+        cur.execute('select part_id from interchange_item '
+                    'where part_id!=%(pid)s and interchange_header_id=%(hid)s',
+                    {'pid': pid, 'hid': hid})
+        for row in cur.fetchall():
+            ipid = row['part_id']
+            ipart = load_part_short(cur, ipid)
+            retval.append(ipart)
+    return retval
+
+
 try:
     cnx = mysql.connector.connect(host=args.db_host, port=args.db_port,
                                   database=args.db_name,
                                   user=args.db_username,
                                   password=args.db_password)
     cursor = cnx.cursor(dictionary=True, buffered=True)
+    cursor2 = cnx.cursor(dictionary=True, buffered=True)
     # Map 'manufacturer_id' => 'manufacturer'.
+    print('Load manufactureres...')
     cursor.execute('select id, name, manfr_type_id, '
                    'parent_manfr_id, not_external '
                    'from manfr')
     mnfr_map = {m['id']: m for m in cursor.fetchall()}
     # Map 'part_type_id' => 'part_type'.
+    print('Load part types...')
     cursor.execute('select id, name from part_type')
-    parttype_map = {pt['id']: pt for pt in cursor.fetchall()}
+    parttypes_map = {pt['id']: pt for pt in cursor.fetchall()}
     # Map 'turbo_type_id' => 'turbo_type'.
+    print('Load turbo types...')
     cursor.execute('select id, name, manfr_id from turbo_type')
     turbotypes_map = {tt['id']: tt for tt in map(normalize_manfrid_in_tt,
                                                  cursor.fetchall())}
     # Map 'turbo_model_id' => 'turbo_model'.
+    print('Load turbo models...')
     cursor.execute('select id, name, turbo_type_id from turbo_model')
     turbomodels_map = {tm['id']: tm for tm in map(normalize_ttid_in_tm,
                                                   cursor.fetchall())}
     # Map 'cool_type_id' => 'cool_type'.
+    print('Load cool types...')
     cursor.execute('select id, name from cool_type')
     cooltypes_map = {ct['id']: ct for ct in cursor.fetchall()}
+    # Map 'part_type_id' => 'list of critical_dimension for the part type'.
+    print('Load critical dimenstions...')
+    cursor.execute('select part_type_id, json_name from crit_dim')
+    crit_dims_map = {}
+    for cd in cursor.fetchall():
+        ptid = cd['part_type_id']
+        ptcd = crit_dims_map.get(ptid, None)
+        if ptcd is None:
+            ptcd = []
+            crit_dims_map[ptid] = ptcd
+        ptcd.append(cd)
     # Export parts.
+    print('Export parts...')
     cursor.execute('select id, part_type_id from part order by id')
     result = []
-    for rec in cursor.fetchall():
+    for rec in tqdm(cursor.fetchall()):
         ptid = rec['part_type_id']
-        sql = SQL_DICT.get(ptid)
-        cursor.execute(sql, {'id': rec['id']})
+        sql = parttype2query(ptid)
+        pid = rec['id']
+        cursor.execute(sql, {'id': pid})
         p = cursor.fetchone()
+        normalize_part(p)
+        boms = load_boms(cursor2, pid)
+        p['boms'] = boms
+        interchanges = load_interchanges(cursor2, pid)
+        p['interchanges'] = interchanges
         result.append(p)
+    cursor2.close()
     cursor.close()
-    print(json.dumps(result, indent=2))
+    print('Writing reslust to a file [{}]...'.format(args.out))
+    with open(args.out, 'wt') as out:
+        out.write(json.dumps(result, indent=2))
+    print('Done.')
 except mysql.connector.Error as err:
     print(err)
 else:
