10bd14275bdbf77182f692322ad91296c056cd96
Dmytro Trunykov <dmytro.trunykov@zorallabs.com>
2017-09-25 14:33:11 +0300

    Dropped obsolete tables for BOMs and Interchanges.

--- metadata/alter.sql
+++ metadata/alter.sql
@@ -1,2 +1,6 @@
-drop table interchange_item;
-drop table interchange_header;
+drop table if exists interchange_item;
+drop table if exists interchange_header;
+drop table if exists bom_descendant;
+drop table if exists bom_alt_item;
+drop table if exists bom_alt_header;
+drop table if exists bom;
