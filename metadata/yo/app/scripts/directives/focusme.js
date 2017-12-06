ab384b40ffabae78a1bf733692602e7adcef5a09
Dmytro Trunykov <dmytro.trunykov@zorallabs.com>
2017-12-06 16:28:00 +0200

    Ticket (Redmine) #283.

---
+++ metadata/yo/app/scripts/directives/focusme.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('ngMetaCrudApp')
+  .directive('focusMe', ['$timeout', '$parse', function ($timeout, $parse) {
+    return {
+      //scope: true,   // optionally create a child scope
+      link: function (scope, element, attrs) {
+        var model = $parse(attrs.focusMe);
+        scope.$watch(model, function (value) {
+          if (value === true) {
+            $timeout(function () {
+              element[0].focus();
+            });
+          }
+        });
+      }
+    };
+  }]);
