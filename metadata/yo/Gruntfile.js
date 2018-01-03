94903b75ed1b1ea794fe2b02184a532ed0044384
Dmytro Trunykov <dmytro.trunykov@zorallabs.com>
2018-01-03 16:24:09 +0200

    Display diffs in the part view.

--- metadata/yo/Gruntfile.js
+++ metadata/yo/Gruntfile.js
@@ -173,6 +173,7 @@ module.exports = function (grunt) {
               47842,  // 'Where used (ancestors)'
               10756, 10757,
               50055,  // Common Component Mapping
+              65254,
               63398, 64449,
               69690, 70079, 70090,   // Tabs: 'Turbos'
               78252
